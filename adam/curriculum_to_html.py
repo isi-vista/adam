22d86025023663ae235c2de7232b78b058fb9152
Jacob Lichtefeld <jalichtefeld@gmail.com>
2019-09-11 18:01:58 -0400

    Reworking class structure

--- adam/curriculum_to_html.py
+++ adam/curriculum_to_html.py
@@ -29,29 +29,26 @@ class CurriculumToHtml:
 
     @staticmethod
     def createbuilder(
-        instance: InstanceGroup, outputdestination: str
+        instances: [InstanceGroup], outputdestination: str
     ) -> "CurriculumToHtml":
         """
 
         Args:
-            instance:
+            instances:
             outputdestination:
 
         Returns:
 
         """
-        instances = instance.instances()
         return CurriculumToHtml(
-            situation=instances[0],
-            linguistics=instances[1],
-            perception=instances[2],
-            outputDestination=outputdestination,
+            outputDestination=outputdestination
         )
 
-    def generate(self, title: str, overwrite: bool) -> int:
+    def _generate(self, instance: InstanceGroup, title: str, overwrite: bool) -> int:
         """
 
         Args:
+            overwrite:
             title:
 
         Returns:
@@ -66,27 +63,52 @@ class CurriculumToHtml:
         html.writelines(self._perceptiontext())
         html.close()
 
-    def _situationtext(self) -> list[str]:
+    def _situationtext(self, situation: HighLevelSemanticsSituation) -> list[str]:
         """
 
         Returns:
 
         """
-        outputtext = ['<div><h2 id="situation">Situation Description']
-
+        outputtext = ['<div><h2 id="situation">Situation Description</h2>\n', '<h3>Objects</h3>\n<ul>']
+        for obj in situation.objects:
+            outputtext.append(f"<li>${obj.ontology_node.handle}</li>")
+            outputtext.append(f"<li>Properties:\n<ul>")
+            for property in obj.properties:
+                outputtext.append(f"<li>${property.handle}</li>")
+            outputtext.append("</ul></li>")
+        outputtext.append("</ul>\n<h3>Actions</h3>\n<ul>")
+        for acts in situation.actions:
+            outputtext.append(f"<li>${acts.action_type.handle}</li>")
+        outputtext.append("</ul>\n<h3>Relations</h3>\n<ul>")
+        for rel in situation.relations:
+            outputtext.append(f"<li>${rel.relation_type.handle} with First Argument: ${rel.first_slot.ontology_node.handle} and ${rel.second_slot.ontology_node.handle}</li>")
+        outputtext.append("</ul>\n<h3>Ontology</h3>\n<ul><li>We should probably determine some way of displaying this but I wasn't sure how?</li></ul>")
         outputtext.append("</div>")
         return outputtext
 
-    def _perceptiontext(self) -> list[str]:
+    def _perceptiontext(self, perception: PerceptualRepresentation[DevelopmentalPrimitivePerceptionFrame]) -> list[str]:
         """
 
         Returns:
 
         """
         outputtext = [
-            '<div><h2 id="perception">Perception Description of the <a href="#situation">Situation</a>'
+            '<div><h2 id="perception">Perception Description of the <a href="#situation">Situation</a></h2>'
         ]
-
+        x = 0
+        for frame in perception.frames:
+            outputtext.append(f"<h3>Frame ${x}</h3>")
+            outputtext.append("<h4>Perceived Objects</h4>\n<ul>")
+            for obj in frame.perceived_objects:
+                outputtext.append(f"<li>${obj.debug_handle}</li>")
+            outputtext.append("</ul>\n<h4>Property Assertions</h4>\n<ul>")
+            for prop in frame.property_assertions:
+                outputtext.append(f"<li>${prop}")
+            outputtext.append("</ul>\n<h4>Relations</h4>\n<ul>")
+            for rel in frame.relations:
+                outputtext.append(f"<li>${rel.relation_type.handle} for ${rel.arg1} and ${rel.arg2}")
+            outputtext.append("</ul>")
+            x = x+1
         outputtext.append("</div>")
         return outputtext
 
@@ -96,9 +118,7 @@ class CurriculumToHtml:
         Returns:
 
         """
-        outputtext = [
-            '<div><h2 id="lingustics">Linguistic Description of the <a href="#situation">Situation</a>'
-        ]
-
-        outputtext.append("</div>")
+        outputtext = ['<div><h2 id="lingustics">Linguistic Description of the <a '
+                      'href="#situation">Situation</a></h2> ',
+                      " ".join(self._linguistics.as_token_sequence()), "</div>"]
         return outputtext
---
+++ tests/curriculum_to_html_test.py
@@ -0,0 +1,6 @@
+from adam import curriculum_to_html
+
+
+def test_simple_curriculum_html():
+    instance =
+    currToHtml = curriculum_to_html.createbuilder(instance, "curriculum.html")
