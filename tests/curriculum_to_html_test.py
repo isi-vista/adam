b1263722e86953272d4846cd9e23d3c2f794d525
Jacob Lichtefeld <jalichtefeld@gmail.com>
2019-09-12 17:57:41 -0400

    Fix CI

--- tests/curriculum_to_html_test.py
+++ tests/curriculum_to_html_test.py
@@ -5,4 +5,3 @@ from adam.curriculum_to_html import CurriculumToHtml
 def test_simple_curriculum_html():
     instances = [EACH_OBJECT_BY_ITSELF_SUB_CURRICULUM]
     CurriculumToHtml.generate(instances, "./", overwrite=True, title="Test Objects")
-    pass
