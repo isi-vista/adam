f4387bdcc8309371367a9632028faf4aecd2c44f
Jacob Lichtefeld <jalichtefeld@gmail.com>
2019-09-12 17:53:06 -0400

    Refactor curriculum_to_html to be static, bare-bones html

--- adam/curriculum_to_html.py
+++ adam/curriculum_to_html.py
@@ -18,8 +18,8 @@ class CurriculumToHtml:
     Class to turn an `InstanceGroup` into an html document
     """
 
+    @staticmethod
     def generate(
-        self,
         instances: List[
             InstanceGroup[
                 HighLevelSemanticsSituation,
@@ -32,16 +32,16 @@ class CurriculumToHtml:
         overwrite: bool = False,
     ) -> int:
         for x in range(len(instances)):  # pylint:disable=consider-using-enumerate
-            self._generate(
+            CurriculumToHtml._generate(
                 instance=instances[x],
-                outputdestination=f"${outputdestination}${title}${x}.html",
-                title=title + f" ${x}",
+                outputdestination=f"{outputdestination}{title}{x}.html",
+                title=title + f" {x}",
                 overwrite=overwrite,
             )
         return 1
 
+    @staticmethod
     def _generate(
-        self,
         instance: InstanceGroup[
             HighLevelSemanticsSituation,
             LinearizedDependencyTree,
@@ -63,101 +63,107 @@ class CurriculumToHtml:
         if os.path.isfile(outputdestination) and not overwrite:
             return 1
         html = open(outputdestination, "w")
-        html.write(f"<h1>${title} - ${instance.name()}</h1>\n")
+        html.write(f"<h1>{title} - {instance.name()}</h1>\n")
         x = 0
         for inst in instance.instances():
             if not isinstance(inst[0], HighLevelSemanticsSituation):
                 raise RuntimeError(
-                    f"Expected the Situation to be HighLevelSemanticsSituation got ${type(inst[0])}"
+                    f"Expected the Situation to be HighLevelSemanticsSituation got {type(inst[0])}"
                 )
             if not isinstance(inst[1], LinearizedDependencyTree):
                 raise RuntimeError(
-                    f"Expected the Lingustics to be LinearizedDependencyTree got ${type(inst[1])}"
+                    f"Expected the Lingustics to be LinearizedDependencyTree got {type(inst[1])}"
                 )
             if not isinstance(inst[2], PerceptualRepresentation) and not isinstance(
                 inst[2].frames, DevelopmentalPrimitivePerceptionFrame
             ):
                 raise RuntimeError(
-                    f"Expected the Perceptual Representation to contain DevelopmentalPrimitivePerceptionFrame got ${type(inst[2].frames)}"
+                    f"Expected the Perceptual Representation to contain DevelopmentalPrimitivePerceptionFrame got {type(inst[2].frames)}"
                 )
-            html.write(f"<h2>Scene ${x}</h2>\n<div>")
-            html.writelines(self._situationtext(inst[0]))
-            html.writelines(self._linguistictext(inst[1]))
-            html.writelines(self._perceptiontext(inst[2]))
-            html.write("</div>")
+            html.write(
+                f'<table>\n<thead><tr><th colspan="3"><h2>Scene {x}</h2>\n</th></tr></thead>'
+                f'<tbody><tr><td><h3 id="situation{x}">Situation Description</h3></td>'
+                f'<td><h3 id="lingustics{x}">Linguistic Descrption</h3></td>'
+                f'<td><h3 id="perception{x}">Perception Description</h3></td></tr>\n<tr>'
+            )
+            html.writelines(CurriculumToHtml._situationtext(inst[0]))
+            html.writelines(CurriculumToHtml._linguistictext(inst[1]))
+            html.writelines(CurriculumToHtml._perceptiontext(inst[2]))
+            html.write("</tr></tbody>")
+            x = x + 1
         html.close()
         return 0
 
-    def _situationtext(self, situation: HighLevelSemanticsSituation):
+    @staticmethod
+    def _situationtext(situation: HighLevelSemanticsSituation):
         """
 
         Returns:
 
         """
-        outputtext = [
-            '<div><h2 id="situation">Situation Description</h2>\n',
-            "<h3>Objects</h3>\n<ul>",
-        ]
+        outputtext = [f"<td>\n", "<h4>Objects</h4>\n<ul>"]
         for obj in situation.objects:
-            outputtext.append(f"<li>${obj.ontology_node.handle}</li>")
-            outputtext.append(f"<li>Properties:\n<ul>")
-            for prop in obj.properties:
-                outputtext.append(f"<li>${prop.handle}</li>")
-            outputtext.append("</ul></li>")
-        outputtext.append("</ul>\n<h3>Actions</h3>\n<ul>")
-        for acts in situation.actions:
-            outputtext.append(f"<li>${acts.action_type.handle}</li>")
-        outputtext.append("</ul>\n<h3>Relations</h3>\n<ul>")
-        for rel in situation.relations:
-            outputtext.append(
-                f"<li>${rel.relation_type.handle} with First Argument: ${rel.first_slot.ontology_node.handle} and ${rel.second_slot.ontology_node.handle}</li>"
-            )
-        outputtext.append(
-            "</ul>\n<h3>Ontology</h3>\n<ul><li>We should probably determine some way of displaying this but I wasn't sure how?</li></ul>"
-        )
-        outputtext.append("</div>")
+            outputtext.append(f"<li>{obj.ontology_node.handle}</li>")
+            # Reintroduce Properties as [] around the object
+            # outputtext.append(f"<li>Properties:\n<ul>")
+            # for prop in obj.properties:
+            #    outputtext.append(f"<li>{prop.handle}</li>")
+            # outputtext.append("</ul></li>")
+        outputtext.append("</ul>")
+        if not situation.actions:
+            outputtext.append("<h4>Actions</h4>\n<ul>")
+            for acts in situation.actions:
+                outputtext.append(f"<li>{acts.action_type.handle}</li>")
+            outputtext.append("</ul>")
+        if not situation.relations:
+            outputtext.append("<h4>Relations</h4>\n<ul>")
+            for rel in situation.relations:
+                outputtext.append(
+                    f"<li>{rel.relation_type.handle}({rel.first_slot.ontology_node.handle},{rel.second_slot.ontology_node.handle})</li>"
+                )
+            outputtext.append("</ul>")
+        outputtext.append("</td>")
         return outputtext
 
+    @staticmethod
     def _perceptiontext(
-        self, perception: PerceptualRepresentation[DevelopmentalPrimitivePerceptionFrame]
+        perception: PerceptualRepresentation[DevelopmentalPrimitivePerceptionFrame]
     ):
         """
 
         Returns:
 
         """
-        outputtext = [
-            '<div><h2 id="perception">Perception Description of the <a href="#situation">Situation</a></h2>'
-        ]
+        outputtext = [f"<td>"]
         x = 0
         for frame in perception.frames:
-            outputtext.append(f"<h3>Frame ${x}</h3>")
-            outputtext.append("<h4>Perceived Objects</h4>\n<ul>")
+            outputtext.append(f"<h4>Frame {x}</h4>")
+            outputtext.append("<h5>Perceived Objects</h5>\n<ul>")
             for obj in frame.perceived_objects:
-                outputtext.append(f"<li>${obj.debug_handle}</li>")
-            outputtext.append("</ul>\n<h4>Property Assertions</h4>\n<ul>")
-            for prop in frame.property_assertions:
-                outputtext.append(f"<li>${prop}")
-            outputtext.append("</ul>\n<h4>Relations</h4>\n<ul>")
-            for rel in frame.relations:
-                outputtext.append(
-                    f"<li>${rel.relation_type.handle} for ${rel.arg1} and ${rel.arg2}"
-                )
+                outputtext.append(f"<li>{obj.debug_handle}</li>")
             outputtext.append("</ul>")
+            if not frame.property_assertions:
+                outputtext.append("<h5>Property Assertions</h5>\n<ul>")
+                for prop in frame.property_assertions:
+                    outputtext.append(f"<li>{prop}")
+                outputtext.append("</ul>")
+            if not frame.relations:
+                outputtext.append("<h5>Relations</h5>\n<ul>")
+                for rel in frame.relations:
+                    outputtext.append(
+                        f"<li>{rel.relation_type.handle}({rel.arg1},{rel.arg2})"
+                    )
+                outputtext.append("</ul>")
             x = x + 1
-        outputtext.append("</div>")
+        outputtext.append("</td>")
         return outputtext
 
-    def _linguistictext(self, linguistic: LinearizedDependencyTree):
+    @staticmethod
+    def _linguistictext(linguistic: LinearizedDependencyTree):
         """
 
         Returns:
 
         """
-        outputtext = [
-            '<div><h2 id="lingustics">Linguistic Description of the <a '
-            'href="#situation">Situation</a></h2> ',
-            " ".join(linguistic.as_token_sequence()),
-            "</div>",
-        ]
+        outputtext = [f"<td>", " ".join(linguistic.as_token_sequence()), "</td>"]
         return outputtext
--- tests/curriculum_to_html_test.py
+++ tests/curriculum_to_html_test.py
@@ -1,48 +1,8 @@
-from typing import Tuple
-
-from immutablecollections import ImmutableSet
-
-from adam import curriculum_to_html
-from adam.experiment.instance_group import ExplicitWithSituationInstanceGroup
-from adam.language_specific.english.english_language_generator import (
-    SimpleRuleBasedEnglishLanguageGenerator,
-)
-from adam.language_specific.english.english_phase_1_lexicon import (
-    GAILA_PHASE_1_ENGLISH_LEXICON,
-)
-from adam.ontology.phase1_ontology import GAILA_PHASE_1_ONTOLOGY, TABLE
-from adam.perception.high_level_semantics_situation_to_developmental_primitive_perception import (
-    HighLevelSemanticsSituationToDevelopmentalPrimitivePerceptionGenerator,
-)
-from adam.random_utils import FixedIndexChooser
-from adam.situation import SituationObject
-from adam.situation.high_level_semantics_situation import HighLevelSemanticsSituation
-
-_SIMPLE_GENERATOR = SimpleRuleBasedEnglishLanguageGenerator(
-    ontology_lexicon=GAILA_PHASE_1_ENGLISH_LEXICON
-)
-
-_PERCEPTION_GENERATOR = HighLevelSemanticsSituationToDevelopmentalPrimitivePerceptionGenerator(
-    GAILA_PHASE_1_ONTOLOGY
-)
+from adam.curriculum.phase1_curriculum import EACH_OBJECT_BY_ITSELF_SUB_CURRICULUM
+from adam.curriculum_to_html import CurriculumToHtml
 
 
 def test_simple_curriculum_html():
-    instances = []
-    table = SituationObject(TABLE)
-    situation = HighLevelSemanticsSituation(
-        ontology=GAILA_PHASE_1_ONTOLOGY,
-        objects=ImmutableSet[table],
-        # actions=ImmutableSet[],
-        # relations=ImmutableSet[]
-    )
-    linguistics = _SIMPLE_GENERATOR.generate_language(situation, FixedIndexChooser(0))
-    perception = _PERCEPTION_GENERATOR.generate_perception(
-        situation=situation, chooser=FixedIndexChooser(0)
-    )
-    group = ExplicitWithSituationInstanceGroup(
-        name="Test Group", instances=Tuple[Tuple[situation, linguistics, perception]]
-    )
-    instances.append(group)
-    html_exporter = curriculum_to_html.CurriculumToHtml()
-    html_exporter.generate(instances, "./", overwrite=True, title="Test Objects")
+    instances = [EACH_OBJECT_BY_ITSELF_SUB_CURRICULUM]
+    CurriculumToHtml.generate(instances, "./", overwrite=True, title="Test Objects")
+    pass
