912674b5e7cf40a0ec29f8e8b52ac809a5e29694
Ryan Gabbard <gabbard@isi.edu>
2019-09-19 08:53:01 -0400

    Make index of instance groups in curriculum dump (#179)
    Closes #177

--- tests/curriculum_to_html_test.py
+++ tests/curriculum_to_html_test.py
@@ -5,4 +5,4 @@ from adam.curriculum_to_html import CurriculumToHtmlDumper
 def test_simple_curriculum_html(tmp_path):
     instances = [EACH_OBJECT_BY_ITSELF_SUB_CURRICULUM]
     html_dumper = CurriculumToHtmlDumper()
-    html_dumper.dump_to_html(instances, output_destination=tmp_path, title="Test Objects")
+    html_dumper.dump_to_html(instances, output_directory=tmp_path, title="Test Objects")
