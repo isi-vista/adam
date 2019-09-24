a52cc393d65754e96aa633a1a9d894ae3b69f858
Jacob Lichtefeld <jalichtefeld@gmail.com>
2019-09-24 12:31:11 -0400

    Describe `size of` relationships

--- adam/curriculum_to_html.py
+++ adam/curriculum_to_html.py
@@ -128,24 +128,27 @@ class CurriculumToHtmlDumper:
             html_out.write(f"<head>\n\t<style>{CSS}\n\t</style>\n</head>")
             html_out.write(f"\n<body>\n\t<h1>{title} - {instance_group.name()}</h1>")
             html_out.write("\n<h2>How to read</h2>")
-            html_out.write(f"\n<p>Our curriculum is designed in two levels: The objective, here - {instance_group.name()}")
+            html_out.write(
+                f"\n<p>Our curriculum is designed in two levels: The objective, here - {instance_group.name()}"
+            )
             html_out.write(
                 "\n and a set of scenes for each objective. The scenes are in the shaded table below."
                 "\n For each scene, we display: </p>"
                 "\n\t<li>Situation (column 1), which is used by the curriculum developer to specify key objects and actions,"
-                "\n but is unseen by the learner</li>."  
-                "\n\t<li>Language (column 2), the text presented to the learner</li>."  
+                "\n but is unseen by the learner</li>."
+                "\n\t<li>Language (column 2), the text presented to the learner</li>."
                 "\n\t<li>Learner Perception (column 3), a textual representation of the perceptual features"
-                "\n presented to the learner</li>")
+                "\n presented to the learner</li>"
+            )
             html_out.write(
                 "<p> The perceptual features described in column 3 are presented to the learner in a"
                 "\n machine interpretable fashion (e.g. as attributes, variables, spatial properties). Here "
-                "\n to assist interpretation of the curriculum we serialize them as debugging text strings." 
+                "\n to assist interpretation of the curriculum we serialize them as debugging text strings."
                 "\n The following are descriptions of some of the perceptual inputs (and their serialization).</p>"
             )
 
             html_out.write(
-                "\n\t<li>Part of relations (e.g. a head being a part of a person) are represented with the parts 
+                "\n\t<li>Part of relations (e.g. a head being a part of a person) are represented with the parts"
                 "\n\t appearing as sub-bullets of an object (e.g. head as a sub-bullet under person).</li>"
                 "\n\t<li>Numeric indices (e.g. person_0 person_1 leg_0, leg_1) distinguish between objects of the"
                 "\n\t same class. </li>"
@@ -153,8 +156,9 @@ class CurriculumToHtmlDumper:
                 "\n\t<li>SpatialPath describes the path that a object takes during a situation. The path"
                 "\n\t specifies if (and how) an object changes orientation. </li>"
                 "\n\t<li>State changes or relations are described with arrows. Relations that are instatiated"
-                "\n\t during the scene are preceded by 'Ø --->'. Those that cease to exist during a scene are" 
-                "\n\t followed by '---> Ø'  </li>"                
+                "\n\t during the scene are preceded by 'Ø --->'. Those that cease to exist during a scene are"
+                "\n\t followed by '---> Ø'  </li>"
+                "\n\t<li>Size relationships are indicated by the '>' symbol. We only display the bigger than size relation but the inverse is also a part of the relationship set.</li>"
             )
             for (instance_number, (situation, dependency_tree, perception)) in enumerate(
                 instance_group.instances()
