24ac6060de967b5f038b209a0283e3963e95b0b2
Ryan Gabbard <gabbard@isi.edu>
2019-10-21 16:19:37 -0400

    Convenience method for exhaustive tests

--- tests/adam_test_utils.py
+++ tests/adam_test_utils.py
@@ -1,7 +1,13 @@
+from typing import Iterable
+
+from adam.ontology.phase1_ontology import GAILA_PHASE_1_ONTOLOGY
 from adam.perception import ObjectPerception
 from adam.perception.developmental_primitive_perception import (
     DevelopmentalPrimitivePerceptionFrame,
 )
+from adam.random_utils import RandomChooser
+from adam.situation.high_level_semantics_situation import HighLevelSemanticsSituation
+from adam.situation.templates.phase1_templates import Phase1SituationTemplate, all_possible
 
 
 def perception_with_handle(
@@ -13,3 +19,11 @@ def perception_with_handle(
     raise RuntimeError(
         f"Could not find object perception with handle {handle} " f"in {frame}"
     )
+
+
+def all_possible_test(template: Phase1SituationTemplate) -> Iterable[HighLevelSemanticsSituation]:
+    """
+    Shorcut for `all_possible` with the GAILA phase 1 ontology and a `RandomChooser` with seed 0.
+    """
+    return all_possible(template, chooser=RandomChooser.for_seed(0),
+                        ontology=GAILA_PHASE_1_ONTOLOGY)
