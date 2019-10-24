ff1394a59ef15f6567a7346fc1f3f381bf92d94c
Ryan Gabbard <gabbard@isi.edu>
2019-10-24 17:59:44 -0400

    Clean up graph matching test

--- tests/perception/perception_graph_test.py
+++ tests/perception/perception_graph_test.py
@@ -1,6 +1,7 @@
 from itertools import chain
 from pathlib import Path
 
+import pytest
 from more_itertools import first
 
 from adam.curriculum.phase1_curriculum import _standard_object
@@ -16,7 +17,7 @@ from adam.ontology.phase1_ontology import (
     above,
     bigger_than,
     on,
-)
+    CAR, _CAR_SCHEMA)
 from adam.ontology.structural_schema import ObjectStructuralSchema
 from adam.perception.high_level_semantics_situation_to_developmental_primitive_perception import (
     GAILA_PHASE_1_PERCEPTION_GENERATOR,
@@ -30,8 +31,16 @@ from adam.situation.templates.phase1_templates import (
 )
 from adam_test_utils import all_possible_test
 
+def test_car_on_table():
+    do_object_on_table_test(CAR, _CAR_SCHEMA, BIRD)
 
-def test_box_on_table():
+@pytest.mark.skip(msg="Slow graph matching test disabled.")
+def test_inanimate_objects():
+    """
+    This test is slow, so it is disabled by default.
+    Returns:
+
+    """
     for object_ in GAILA_PHASE_1_ONTOLOGY.nodes_with_properties(
         INANIMATE_OBJECT, banned_properties=[LIQUID, IS_BODY_PART]
     ):
@@ -39,14 +48,14 @@ def test_box_on_table():
             schemata = GAILA_PHASE_1_ONTOLOGY.structural_schemata(object_)
             if len(schemata) == 1 and object_ != GROUND:
                 print(f"Matching {object_}")
-                if test_object_on_table(object_, first(schemata), BIRD):
+                if do_object_on_table_test(object_, first(schemata), BIRD):
                     print(f"{object_} passed")
                 else:
                     print(f"{object_} failed")
 
 
-def test_object_on_table(
-    object_ontology_node: OntologyNode,
+def do_object_on_table_test(
+    object_type_to_match: OntologyNode,
     object_schema: ObjectStructuralSchema,
     negative_object_ontology_node: OntologyNode,
 ):
@@ -54,43 +63,46 @@ def test_object_on_table(
     Tests the `PerceptionGraphMatcher` can match simple objects.
     """
     # we create four situations:
-    # a truck above or under a table with color red or blue
+    # a object_to_match above or under a table with color red or blue
     color = color_variable("color")
-    truck = object_variable(
-        debug_handle="truck_0", root_node=object_ontology_node, added_properties=[color]
+    object_to_match = object_variable(
+        debug_handle=object_type_to_match.handle, root_node=object_type_to_match, \
+                                                               added_properties=[color]
     )
     table = _standard_object("table_0", TABLE)
 
-    truck_on_table_template = Phase1SituationTemplate(
-        "truck-on-table",
-        salient_object_variables=[truck, table],
-        asserted_always_relations=[bigger_than(table, truck), on(truck, table)],
+    object_on_table_template = Phase1SituationTemplate(
+        "object_to_match-on-table",
+        salient_object_variables=[object_to_match, table],
+        asserted_always_relations=[bigger_than(table, object_to_match), on(object_to_match, table)],
     )
 
-    truck_under_table_template = Phase1SituationTemplate(
-        "truck-under-table",
-        salient_object_variables=[truck, table],
-        asserted_always_relations=[bigger_than(table, truck), above(table, truck)],
+    object_under_table_template = Phase1SituationTemplate(
+        "object_to_match-under-table",
+        salient_object_variables=[object_to_match, table],
+        asserted_always_relations=[bigger_than(table, object_to_match), above(table, object_to_match)],
     )
 
-    # We test that a perceptual pattern for "truck" matches in all four cases.
-    truck_pattern = PerceptionGraphPattern.from_schema(object_schema)
+    # We test that a perceptual pattern for "object_to_match" matches in all four cases.
+    object_to_match_pattern = PerceptionGraphPattern.from_schema(object_schema)
 
-    situations_with_truck = chain(
-        all_possible_test(truck_on_table_template),
-        all_possible_test(truck_under_table_template),
+    situations_with_object_to_match = chain(
+        all_possible_test(object_on_table_template),
+        all_possible_test(object_under_table_template),
     )
 
     out_dir = Path("/Users/gabbard/tmp")
 
-    for (idx, situation_with_truck) in enumerate(situations_with_truck):
+    for (idx, situation_with_object) in enumerate(situations_with_object_to_match):
         perception = GAILA_PHASE_1_PERCEPTION_GENERATOR.generate_perception(
-            situation_with_truck, chooser=RandomChooser.for_seed(0)
+            situation_with_object, chooser=RandomChooser.for_seed(0)
         )
         perception_graph = to_perception_graph(perception.frames[0])
-        perception_graph.render_to_file(f"truck {idx}", out_dir / f"truck-{idx}.pdf")
-        truck_pattern.render_to_file(f"truck pattern", out_dir / "truck_pattern.pdf")
-        matcher = truck_pattern.matcher(perception_graph)
+        #perception_graph.render_to_file(f"object_to_match {idx}", out_dir / f"object_to_match
+        # -{idx}.pdf")
+        #object_to_match_pattern.render_to_file(f"object_to_match pattern", out_dir /
+        # "object_to_match_pattern.pdf")
+        matcher = object_to_match_pattern.matcher(perception_graph)
         # debug_matching = matcher.debug_matching(
         #    use_lookahead_pruning=False, render_match_to=Path("/Users/gabbard/tmp")
         # )
@@ -98,35 +110,35 @@ def test_object_on_table(
         if not result:
             return False
 
-    # Now let's create the same situations, but substitute a bird for a truck.
-    bird = object_variable(
-        debug_handle="bird_0",
+    # Now let's create the same situations, but substitute a negative_object for a object_to_match.
+    negative_object = object_variable(
+        debug_handle=negative_object_ontology_node.handle,
         root_node=negative_object_ontology_node,
         added_properties=[color],
     )
-    bird_on_table_template = Phase1SituationTemplate(
-        "bird-on-table",
-        salient_object_variables=[bird, table],
-        asserted_always_relations=[bigger_than(table, bird), on(bird, table)],
+    negative_object_on_table_template = Phase1SituationTemplate(
+        "negative_object-on-table",
+        salient_object_variables=[negative_object, table],
+        asserted_always_relations=[bigger_than(table, negative_object), on(negative_object, table)],
     )
 
-    bird_under_table_template = Phase1SituationTemplate(
-        "bird-under-table",
-        salient_object_variables=[bird, table],
-        asserted_always_relations=[bigger_than(table, bird), above(table, bird)],
+    negative_object_under_table_template = Phase1SituationTemplate(
+        "negative_object-under-table",
+        salient_object_variables=[negative_object, table],
+        asserted_always_relations=[bigger_than(table, negative_object), above(table, negative_object)],
     )
 
-    situations_with_bird = chain(
-        all_possible_test(bird_on_table_template),
-        all_possible_test(bird_under_table_template),
+    situations_with_negative_object = chain(
+        all_possible_test(negative_object_on_table_template),
+        all_possible_test(negative_object_under_table_template),
     )
 
     # The pattern should now fail to match.
-    for situation_with_bird in situations_with_bird:
+    for situation_with_negative_object in situations_with_negative_object:
         perception = GAILA_PHASE_1_PERCEPTION_GENERATOR.generate_perception(
-            situation_with_bird, chooser=RandomChooser.for_seed(0)
+            situation_with_negative_object, chooser=RandomChooser.for_seed(0)
         )
         perception_graph = to_perception_graph(perception.frames[0])
-        if any(truck_pattern.matcher(perception_graph).matches()):
+        if any(object_to_match_pattern.matcher(perception_graph).matches()):
             return False
     return True
