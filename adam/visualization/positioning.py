47351e5bd85d752b39a27b43f8ecf751a625f8d7
Chris Jenkins <cjenkins@isi.edu>
2019-11-20 18:04:59 -0500

    dubious pytorch setup

--- adam/visualization/positioning.py
+++ adam/visualization/positioning.py
@@ -1,29 +1,23 @@
 import numpy as np
 from numpy import ndarray
 
-from typing import List
+from typing import List, Any
 
 from adam.visualization.utils import BoundingBox, min_max_projection
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.autograd import Function
 
 
 def main() -> None:
     bb = BoundingBox.from_center_point(np.array([0, 0, 0]))
     midpoint_minimization(bb)
 
-    assert False
-
-    # standard form:
-    # minimize: x    (1/2) x^(T) Px + q^(T)x
-
-    # subject to: Gx element-wise< h
-    #  " " "    : Ax = b
-
-    # objective function:
-
-    # constraints:
-
 
 class PositionSolver:
+    # constants to eventually use
     POSITION_CONSTRAINTS = {
         "X_RANGE": (-10.0, 10.0),
         "Y_RANGE": (-10.0, 5.0),
@@ -39,97 +33,156 @@ class PositionSolver:
         self.positions = points
 
 
-class RelativePositionSolver:
-    pass
+class PositioningModel(nn.Module):
+    def __init__(self, static_bb):
+        super().__init__()
+        device = torch.device("cpu")
+        dtype = torch.float
+        torch.random.manual_seed(2014)
+
+        self.static_bb = static_bb
+        self.static_bb_faces = torch.tensor(
+            self.static_bb.all_face_normals(), dtype=dtype
+        )
+        print(f"static bb faces: {self.static_bb_faces}")
+
+        static_bb_corners = torch.tensor(self.static_bb.all_corners(), dtype=dtype)
+        print(f"static bb corners: {static_bb_corners}")
+
+        self.static_bb_min_max_projections = torch.tensor(
+            [
+                min_max_projection(static_bb_corners, face)
+                for face in self.static_bb_faces
+            ],
+            dtype=dtype,
+        )
+
+        print(f"static_bb min/max projections: {self.static_bb_min_max_projections}")
+
+        # box variable, initialized to a 3d position w/ normal distribution around 0,0,0 stdev (3, 1, 1)
+        self.center = torch.normal(
+            torch.tensor([0, 0, 0], device=device, dtype=dtype),
+            torch.tensor([3, 1, 1], device=device, dtype=dtype),
+        )
+        # self.center = torch.zeros(3, dtype=dtype, device=device)
+        self.center.requires_grad_(True)
+        self.center = nn.Parameter(self.center)
+        print(self.center)
+
+        self.corner_matrix = torch.tensor(
+            [
+                [-1, -1, -1],
+                [1, -1, -1],
+                [-1, 1, -1],
+                [1, 1, -1],
+                [-1, -1, 1],
+                [1, -1, 1],
+                [-1, 1, 1],
+                [1, 1, 1],
+            ],
+            dtype=dtype,
+        )
+        self.minkowski()
+
+    def minkowski(self):
+        # TODO: see if requires_grad=True is needed for each of these intermediate tensors
+        print(f"center: {self.center}")
+        # get all corners for current center
+        corners = torch.ones((8, 3)) * self.center + self.corner_matrix
+        print(f"corners.shape: {corners.shape}")
+        # project corners onto faces from static bb (output shape (3, 8, 1) )
+
+        projections = torch.tensor(
+            [corners[j].dot(self.static_bb_faces[i]) for i in range(3) for j in range(8)],
+            dtype=torch.float,
+        ).reshape((3, 8, 1))
+        print(f"corner projections shape: {projections.shape}")
+        print(f"corner projections: {projections}")
+
+        face_min_max = torch.tensor(
+            [[torch.min(projections[i]), torch.max(projections[i])] for i in range(3)],
+            dtype=torch.float,
+        )
+        print(f"face min-maxes shape: {face_min_max.shape}")  # (3, 2)
+        print(f"face min-maxes: {face_min_max}")
+
+        # compute overlap between the parameter's min/maxes and the static ones
+
+        overlap_ranges = torch.tensor(
+            [
+                [
+                    torch.max(
+                        face_min_max[i][0], self.static_bb_min_max_projections[i][0]
+                    ),
+                    torch.min(
+                        face_min_max[i][1], self.static_bb_min_max_projections[i][1]
+                    ),
+                ]
+                for i in range(3)
+            ],
+            dtype=torch.float,
+        )
+
+        print(f"shape of overlap ranges: {overlap_ranges.shape}")
+        print(f" overlap ranges: {overlap_ranges}")
+
+        # condense this down in to a scalar for each of the 3 (x, y, z) dimensions:
+        # to represent the degree of overlap / separation
+
+        overlap_distance = torch.tensor(
+            [overlap_ranges[i][0] - overlap_ranges[i][1] for i in range(3)],
+            dtype=torch.float,
+        )
+
+        print(f"shape of overlap distance: {overlap_distance.shape}")  # should be (3, 1)
+        print(f"overlap distance: {overlap_distance}")
+
+        # if ANY element in overlap_distance is positive, the minimum positive value
+        # is the separation distance
+
+        # otherwise the penetration distance is the maximum negative value
+        # (the smallest translation that would disentangle the two
+
+    def forward(self, *input: Any, **kwargs: Any) -> Any:
+        return self.center.dist(torch.tensor([0, 0, 0], dtype=torch.float, device="cpu"))
+        # bb = BoundingBox.from_center_point(self.center.data.numpy())
+        # dist = bb.minkowski_diff_distance(self.static_bb)
+        # if dist <= 0:
+        #     return torch.tensor([0.0], requires_grad=True)
+        # else:
+        #     return torch.tensor([dist], requires_grad=True)
 
 
 def midpoint_minimization(relative_bb: BoundingBox):
-    # objective function:
 
-    # quadratic components: none
-    # P = matrix(np.diag(np.array([0, 0, 0])), tc="d")
-    # linear coefficients: all ones
-    # q = matrix(np.array([1, 1, 1]), tc="d")
+    model = PositioningModel(relative_bb).to("cpu")
 
-    relative_bb_faces = relative_bb.all_face_normals()
+    optimizer = optim.SGD(model.parameters(), lr=0.01, momentum=0.5)
 
-    relative_bb_corners = relative_bb.all_corners()
+    loss_fn = nn.MSELoss()
 
-    relative_bb_min_max_projections = [
-        min_max_projection(relative_bb_corners, face) for face in relative_bb_faces
-    ]
+    y = torch.tensor([0.0])  # no collision
 
-    print(relative_bb_min_max_projections)
+    # training:
+    print("\n\nTraining\n")
 
-    corner_matrix = np.array(
-        [
-            (-1, -1, -1),
-            (1, -1, -1),
-            (-1, 1, -1),
-            (1, 1, -1),
-            (-1, -1, 1),
-            (1, -1, 1),
-            (-1, 1, 1),
-            (1, 1, 1),
-        ]
-    )
+    iterations = 10
+    for _ in range(iterations):
+        model.train()  # set model to train mode
+        # error = NonIntersection.apply(center, relative_bb)
+        print(f"params: {model.state_dict()}")
+        y_hat = model()
+        print(f"error: {y_hat}")
+        print(type(y_hat))
+        # loss = loss_fn(y, y_hat)
+        loss = y_hat ** 2
 
-    print("np test situation")
-    print(corner_matrix + np.array([1.0, 1.0, 1.0]))
+        # automagically gets something passed to it?
+        loss.backward()
+        optimizer.step()
+        optimizer.zero_grad()
 
-    # should have a variable representing center of box
-    box_a_center = np.array((3,))
-
-    # idea of an objective function: minimizing distance from origin
-    # keep in mind though... for abs() to be differentiable it has to be broken up into parts
-    # f = abs(box_a_center[0]) + abs(box_a_center[1]) + abs(box_a_center[2])
-
-    # constraints:
-    # c_x_1 = box_a_center[0] <= 10.0
-    #
-    # c_x_2 = box_a_center[0] >= -10.0
-    # c_y_1 = box_a_center[1] <= 5.0
-    # c_y_2 = box_a_center[1] >= -5.0
-    # c_z_1 = box_a_center[2] <= 2.0
-    # c_z_2 = box_a_center[2] >= 0.0
-
-    # intersection constraints:
-    print(corner_matrix.size)
-
-    center_shaped = np.ones((8, 3)) * box_a_center
-
-    print(f"center_shaped type: {type(center_shaped)}")
-
-    # corner0 = corner_shaped + corner_matrix[0]
-    corners = center_shaped + corner_matrix
-    print(f"type of corners: {type(corners)}")
-    print(f"type of a corners element: {type(corners[0])}")
-
-    print(corners[0].size)
-    print(relative_bb_faces[0].size)
-    print(f"corner shape: {corners[0].shape}")
-    print(f"relative_bb_face shape: {relative_bb_faces[0].shape}")
-    projections = [
-        np.dot(corners[j], relative_bb_faces[i]) for i in range(3) for j in range(8)
-    ]
-
-    print(len(projections))
-    print(f"{projections[0].shape}")
-    print(f"{projections[0].size}")
-
-    # collide_constraint = (
-    #     cvxopt.modeling.min(projections[0], relative_bb_min_max_projections[0][1])
-    #     - cvxopt.modeling.max(projections[1], relative_bb_min_max_projections[0][0])
-    #     <= 0
-    # )
-
-    # prob = op(
-    #     f, [c_x_1, c_x_2, c_y_1, c_y_2, c_z_1, c_z_2, collide_constraint], "test_prob"
-    # )
-    # prob.solve()
-    # print(prob.status)
-
-    # G =
+        print(loss.item())
 
 
 if __name__ == "__main__":
