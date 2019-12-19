9ebc0628251ce27dd3858e9b3c69b995097ff436
Ryan Gabbard <gabbard@isi.edu>
2019-12-18 21:49:12 -0500

    Make learner used for experiment logging configurable

--- adam/experiment/m6_experiments.py
+++ adam/experiment/m6_experiments.py
@@ -1,9 +1,12 @@
+from typing import Callable
+
 from vistautils.parameters import Parameters
 from vistautils.parameters_only_entrypoint import parameters_only_entry_point
 
 from adam.curriculum.m6_curriculum import make_m6_curriculum
 from adam.experiment import execute_experiment, Experiment
 from adam.experiment.observer import LearningProgressHtmlLogger
+from adam.learner import LanguageLearner
 from adam.learner.pursuit import PursuitLanguageLearner
 from adam.random_utils import RandomChooser
 
@@ -22,9 +25,7 @@ def main(params: Parameters) -> None:
         Experiment(
             name="static-prepositions",
             training_stages=make_m6_curriculum(),
-            learner_factory=lambda: PursuitLanguageLearner.from_parameters(
-                params.namespace("pursuit")
-            ),
+            learner_factory=learner_factory_from_params(params),
             pre_example_training_observers=[logger.pre_observer()],
             post_example_training_observers=[logger.post_observer()],
             test_instance_groups=[],
@@ -32,6 +33,16 @@ def main(params: Parameters) -> None:
             sequence_chooser=RandomChooser.for_seed(0),
         )
     )
+
+
+def learner_factory_from_params(
+    params: Parameters
+) -> Callable[[], LanguageLearner]:  # type: ignore
+    learner_type = params.string("learner", ["pursuit"])
+    if learner_type == "pursuit":
+        return lambda: PursuitLanguageLearner.from_parameters(params.namespace("pursuit"))
+    else:
+        raise RuntimeError("can't happen")
 
 
 if __name__ == "__main__":
