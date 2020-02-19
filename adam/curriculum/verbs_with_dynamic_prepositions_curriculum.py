b7cedb51b3340ab9e9f15ef1caa212593cea330c
Jacob L <jalichtefeld@gmail.com>
2020-02-19 16:02:02 -0500

    Add "go" with prepositions

--- adam/curriculum/verbs_with_dynamic_prepositions_curriculum.py
+++ adam/curriculum/verbs_with_dynamic_prepositions_curriculum.py
@@ -27,6 +27,7 @@ from adam.ontology.phase1_ontology import (
     ANIMATE,
     INANIMATE_OBJECT,
     CAN_HAVE_THINGS_RESTING_ON_THEM,
+    GO,
 )
 from adam.ontology.phase1_spatial_relations import (
     Region,
@@ -35,6 +36,7 @@ from adam.ontology.phase1_spatial_relations import (
     Direction,
     GRAVITATIONAL_DOWN,
     DISTAL,
+    GRAVITATIONAL_UP,
 )
 from adam.situation import Action
 from adam.situation.templates.phase1_templates import (
@@ -233,6 +235,211 @@ def _push_in_front_of_behind_template(
     )
 
 
+def _go_to_template(
+    agent: TemplateObjectVariable,
+    goal_object: TemplateObjectVariable,
+    background: Iterable[TemplateObjectVariable],
+) -> Phase1SituationTemplate:
+    return Phase1SituationTemplate(
+        f"go_to-{agent.handle}-to-{goal_object.handle}",
+        salient_object_variables=[agent, goal_object],
+        background_object_variables=background,
+        actions=[
+            Action(
+                GO,
+                argument_roles_to_fillers=[
+                    (AGENT, agent),
+                    (GOAL, Region(goal_object, distance=PROXIMAL)),
+                ],
+            )
+        ],
+        gazed_objects=[agent],
+    )
+
+
+def _go_in_template(
+    agent: TemplateObjectVariable,
+    goal_object: TemplateObjectVariable,
+    background: Iterable[TemplateObjectVariable],
+) -> Phase1SituationTemplate:
+    return Phase1SituationTemplate(
+        f"go_in-{agent.handle}-in-{goal_object.handle}",
+        salient_object_variables=[agent, goal_object],
+        background_object_variables=background,
+        actions=[
+            Action(
+                GO,
+                argument_roles_to_fillers=[
+                    (AGENT, agent),
+                    (GOAL, Region(goal_object, distance=INTERIOR)),
+                ],
+            )
+        ],
+        constraining_relations=[bigger_than(goal_object, agent)],
+    )
+
+
+def _go_beside_template(
+    agent: TemplateObjectVariable,
+    goal_object: TemplateObjectVariable,
+    background: Iterable[TemplateObjectVariable],
+    *,
+    is_right: bool,
+) -> Phase1SituationTemplate:
+    return Phase1SituationTemplate(
+        f"go_beside-{agent.handle}-beside-{goal_object.handle}",
+        salient_object_variables=[agent, goal_object],
+        background_object_variables=background,
+        actions=[
+            Action(
+                GO,
+                argument_roles_to_fillers=[
+                    (AGENT, agent),
+                    (
+                        GOAL,
+                        Region(
+                            goal_object,
+                            distance=PROXIMAL,
+                            direction=Direction(
+                                positive=is_right,
+                                relative_to_axis=HorizontalAxisOfObject(
+                                    goal_object, index=0
+                                ),
+                            ),
+                        ),
+                    ),
+                ],
+            )
+        ],
+    )
+
+
+def _go_behind_template(
+    agent: TemplateObjectVariable,
+    goal_object: TemplateObjectVariable,
+    background: Iterable[TemplateObjectVariable],
+    *,
+    is_distal: bool,
+) -> Phase1SituationTemplate:
+    return Phase1SituationTemplate(
+        f"go_behind-{agent.handle}-behind-{goal_object.handle}",
+        salient_object_variables=[agent, goal_object],
+        background_object_variables=background,
+        actions=[
+            Action(
+                GO,
+                argument_roles_to_fillers=[
+                    (AGENT, agent),
+                    (
+                        GOAL,
+                        Region(
+                            goal_object,
+                            distance=DISTAL if is_distal else PROXIMAL,
+                            direction=Direction(
+                                positive=False,
+                                relative_to_axis=FacingAddresseeAxis(goal_object),
+                            ),
+                        ),
+                    ),
+                ],
+            )
+        ],
+    )
+
+
+def _go_in_front_of_template(
+    agent: TemplateObjectVariable,
+    goal_object: TemplateObjectVariable,
+    background: Iterable[TemplateObjectVariable],
+    *,
+    is_distal,
+) -> Phase1SituationTemplate:
+    return Phase1SituationTemplate(
+        f"go_in_front_of-{agent.handle}-behind-{goal_object.handle}",
+        salient_object_variables=[agent, goal_object],
+        background_object_variables=background,
+        actions=[
+            Action(
+                GO,
+                argument_roles_to_fillers=[
+                    (AGENT, agent),
+                    (
+                        GOAL,
+                        Region(
+                            goal_object,
+                            distance=DISTAL if is_distal else PROXIMAL,
+                            direction=Direction(
+                                positive=True,
+                                relative_to_axis=FacingAddresseeAxis(goal_object),
+                            ),
+                        ),
+                    ),
+                ],
+            )
+        ],
+    )
+
+
+def _go_over_template(
+    agent: TemplateObjectVariable,
+    goal_object: TemplateObjectVariable,
+    background: Iterable[TemplateObjectVariable],
+    *,
+    is_distal: bool,
+) -> Phase1SituationTemplate:
+    return Phase1SituationTemplate(
+        f"go_over-{agent.handle}-over-{goal_object.handle}",
+        salient_object_variables=[agent, goal_object],
+        background_object_variables=background,
+        actions=[
+            Action(
+                GO,
+                argument_roles_to_fillers=[
+                    (AGENT, agent),
+                    (
+                        GOAL,
+                        Region(
+                            goal_object,
+                            distance=DISTAL if is_distal else PROXIMAL,
+                            direction=GRAVITATIONAL_UP,
+                        ),
+                    ),
+                ],
+            )
+        ],
+    )
+
+
+def _go_under_template(
+    agent: TemplateObjectVariable,
+    goal_object: TemplateObjectVariable,
+    background: Iterable[TemplateObjectVariable],
+    *,
+    is_distal: bool,
+) -> Phase1SituationTemplate:
+    return Phase1SituationTemplate(
+        f"go_under-{agent.handle}-under-{goal_object.handle}",
+        salient_object_variables=[agent, goal_object],
+        background_object_variables=background,
+        actions=[
+            Action(
+                GO,
+                argument_roles_to_fillers=[
+                    (AGENT, agent),
+                    (
+                        GOAL,
+                        Region(
+                            goal_object,
+                            distance=DISTAL if is_distal else PROXIMAL,
+                            direction=GRAVITATIONAL_DOWN,
+                        ),
+                    ),
+                ],
+            )
+        ],
+    )
+
+
 def _make_push_with_prepositions(
     num_samples: int = 5, *, noise_objects: int = 0
 ) -> Phase1InstanceGroup:
@@ -332,7 +539,119 @@ def _make_push_with_prepositions(
     )
 
 
+def _make_go_with_prepositions(num_samples: int = 5, *, noise_objects: int = 0):
+    agent = standard_object("agent", THING, required_properties=[ANIMATE])
+    goal_object = standard_object("goal_object")
+    goal_object_hollow = standard_object(
+        "goal_object_hollow", required_properties=[HOLLOW]
+    )
+    background = immutableset(
+        standard_object(f"noise_object_{x}") for x in range(noise_objects)
+    )
+
+    return phase1_instances(
+        "Go + PP",
+        chain(
+            # To
+            flatten(
+                [
+                    sampled(
+                        _go_to_template(agent, goal_object, background),
+                        ontology=GAILA_PHASE_1_ONTOLOGY,
+                        chooser=PHASE1_CHOOSER,
+                        max_to_sample=num_samples,
+                    )
+                ]
+            ),
+            # In
+            flatten(
+                [
+                    sampled(
+                        _go_in_template(agent, goal_object_hollow, background),
+                        ontology=GAILA_PHASE_1_ONTOLOGY,
+                        chooser=PHASE1_CHOOSER,
+                        max_to_sample=num_samples,
+                    )
+                ]
+            ),
+            # Beside
+            flatten(
+                [
+                    sampled(
+                        _go_beside_template(
+                            agent, goal_object, background, is_right=is_right
+                        ),
+                        ontology=GAILA_PHASE_1_ONTOLOGY,
+                        chooser=PHASE1_CHOOSER,
+                        max_to_sample=num_samples,
+                    )
+                    for is_right in BOOL_SET
+                ]
+            ),
+            # Behind
+            flatten(
+                [
+                    sampled(
+                        _go_behind_template(
+                            agent, goal_object, background, is_distal=is_distal
+                        ),
+                        ontology=GAILA_PHASE_1_ONTOLOGY,
+                        chooser=PHASE1_CHOOSER,
+                        max_to_sample=num_samples,
+                    )
+                    for is_distal in BOOL_SET
+                ]
+            ),
+            # In Front Of
+            flatten(
+                [
+                    sampled(
+                        _go_in_front_of_template(
+                            agent, goal_object, background, is_distal=is_distal
+                        ),
+                        ontology=GAILA_PHASE_1_ONTOLOGY,
+                        chooser=PHASE1_CHOOSER,
+                        max_to_sample=num_samples,
+                    )
+                    for is_distal in BOOL_SET
+                ]
+            ),
+            # Over
+            flatten(
+                [
+                    sampled(
+                        _go_over_template(
+                            agent, goal_object, background, is_distal=is_distal
+                        ),
+                        ontology=GAILA_PHASE_1_ONTOLOGY,
+                        chooser=PHASE1_CHOOSER,
+                        max_to_sample=num_samples,
+                    )
+                    for is_distal in BOOL_SET
+                ]
+            ),
+            # Under
+            flatten(
+                [
+                    sampled(
+                        _go_under_template(
+                            agent, goal_object, background, is_distal=is_distal
+                        ),
+                        ontology=GAILA_PHASE_1_ONTOLOGY,
+                        chooser=PHASE1_CHOOSER,
+                        max_to_sample=num_samples,
+                    )
+                    for is_distal in BOOL_SET
+                ]
+            ),
+        ),
+    )
+
+
 def make_verb_with_dynamic_prepositions_curriculum(
     num_samples: int = 5, *, num_noise_objects: int = 0
 ):
-    return [_make_push_with_prepositions(num_samples, noise_objects=num_noise_objects)]
+    return [
+        _make_push_with_prepositions(num_samples, noise_objects=num_noise_objects),
+        _make_go_with_prepositions(num_samples, noise_objects=num_noise_objects),
+    ]
