b0ff4251cc56601822b174c802f2aaf177ad41a4
Liz Lee <elgclee@gmail.com>
2020-02-21 18:59:49 -0500

    Refine templates

--- adam/curriculum/verbs_with_dynamic_prepositions_curriculum.py
+++ adam/curriculum/verbs_with_dynamic_prepositions_curriculum.py
@@ -1,4 +1,4 @@
-from typing import Tuple
+from itertools import chain
 
 from immutablecollections import immutableset, ImmutableSet
 from more_itertools import flatten
@@ -21,6 +21,7 @@ from adam.ontology.phase1_ontology import (
     GAILA_PHASE_1_ONTOLOGY,
     HOLLOW,
     THEME,
+    CAN_HAVE_THINGS_RESTING_ON_THEM,
 )
 from adam.ontology.phase1_spatial_relations import (
     Region,
@@ -104,7 +105,6 @@ def _fall_beside_template(
     background: ImmutableSet[TemplateObjectVariable],
     syntax_hints: ImmutableSet[str],
     is_right: bool,
-    is_distal: bool,
 ) -> Phase1SituationTemplate:
     return Phase1SituationTemplate(
         f"{theme.handle}-falls-(down)-beside-{goal_reference.handle}",
@@ -119,7 +119,7 @@ def _fall_beside_template(
                         GOAL,
                         Region(
                             goal_reference,
-                            distance=DISTAL if is_distal else PROXIMAL,
+                            distance=PROXIMAL,
                             direction=Direction(
                                 positive=is_right,
                                 relative_to_axis=HorizontalAxisOfObject(
@@ -135,12 +135,13 @@ def _fall_beside_template(
     )
 
 
-def _fall_in_front_of_template(
+def _fall_in_front_of_behind_template(
     theme: TemplateObjectVariable,
     goal_reference: TemplateObjectVariable,
     background: ImmutableSet[TemplateObjectVariable],
     syntax_hints: ImmutableSet[str],
     is_distal: bool,
+    is_in_front: bool,
 ) -> Phase1SituationTemplate:
     return Phase1SituationTemplate(
         f"{theme.handle}-falls-(down)-in-front-of-{goal_reference.handle}",
@@ -157,7 +158,7 @@ def _fall_in_front_of_template(
                             goal_reference,
                             distance=DISTAL if is_distal else PROXIMAL,
                             direction=Direction(
-                                positive=True,
+                                positive=is_in_front,
                                 relative_to_axis=FacingAddresseeAxis(
                                     goal_reference, index=0
                                 ),
@@ -171,176 +172,82 @@ def _fall_in_front_of_template(
     )
 
 
-def _fall_behind_template(
-    theme: TemplateObjectVariable,
-    goal_reference: TemplateObjectVariable,
-    background: ImmutableSet[TemplateObjectVariable],
-    syntax_hints: ImmutableSet[str],
-    is_distal: bool,
-) -> Phase1SituationTemplate:
-    return Phase1SituationTemplate(
-        f"{theme.handle}-falls-(down)-behind-{goal_reference.handle}",
-        salient_object_variables=[theme, goal_reference],
-        background_object_variables=background,
-        actions=[
-            Action(
-                FALL,
-                argument_roles_to_fillers=[
-                    (THEME, theme),
-                    (
-                        GOAL,
-                        Region(
+def _make_fall_with_prepositions(
+    num_samples: int = 5, *, noise_objects: int = 0
+) -> Phase1InstanceGroup:
+    theme = standard_object("theme", THING)
+    goal_reference = standard_object("goal_reference", THING)
+    goal_on = standard_object(
+        "goal_on", THING, required_properties=[CAN_HAVE_THINGS_RESTING_ON_THEM]
+    )
+    goal_in = standard_object("goal_in", THING, required_properties=[HOLLOW])
+    background = immutableset(
+        standard_object(f"noise_object_{x}") for x in range(noise_objects)
+    )
+    syntax_hints_options = ([], [USE_ADVERBIAL_PATH_MODIFIER])  # type: ignore
+    return phase1_instances(
+        "Fall + PP",
+        chain(
+            # on
+            flatten(
+                [
+                    sampled(
+                        _fall_on_template(theme, goal_on, background, syntax_hints),
+                        ontology=GAILA_PHASE_1_ONTOLOGY,
+                        chooser=PHASE1_CHOOSER,
+                        max_to_sample=num_samples,
+                    )
+                    for syntax_hints in syntax_hints_options
+                ]
+            ),
+            # in
+            flatten(
+                [
+                    sampled(
+                        _fall_in_template(theme, goal_in, background, syntax_hints),
+                        ontology=GAILA_PHASE_1_ONTOLOGY,
+                        chooser=PHASE1_CHOOSER,
+                        max_to_sample=num_samples,
+                    )
+                    for syntax_hints in syntax_hints_options
+                ]
+            ),
+            # beside
+            flatten(
+                [
+                    sampled(
+                        _fall_beside_template(
+                            theme, goal_reference, background, syntax_hints, is_right
+                        ),
+                        ontology=GAILA_PHASE_1_ONTOLOGY,
+                        chooser=PHASE1_CHOOSER,
+                        max_to_sample=num_samples,
+                    )
+                    for syntax_hints in syntax_hints_options
+                    for is_right in BOOL_SET
+                ]
+            ),
+            # in front of, behind
+            flatten(
+                [
+                    sampled(
+                        _fall_in_front_of_behind_template(
+                            theme,
                             goal_reference,
-                            distance=DISTAL if is_distal else PROXIMAL,
-                            direction=Direction(
-                                positive=False,
-                                relative_to_axis=FacingAddresseeAxis(
-                                    goal_reference, index=0
-                                ),
-                            ),
+                            background,
+                            syntax_hints,
+                            is_distal,
+                            is_in_front,
                         ),
-                    ),
-                ],
-            )
-        ],
-        syntax_hints=syntax_hints,
-    )
-
-
-def _make_fall_on(num_samples: int = 5, *, noise_objects: int = 0) -> Phase1InstanceGroup:
-    theme = standard_object("theme", THING)
-    goal_reference = standard_object("goal_reference", THING)
-    background = immutableset(
-        standard_object(f"noise_object_{x}") for x in range(noise_objects)
-    )
-    syntax_hints_options = ([], [USE_ADVERBIAL_PATH_MODIFIER])  # type: ignore
-    return phase1_instances(
-        "Fall On",
-        flatten(
-            [
-                sampled(
-                    _fall_on_template(theme, goal_reference, background, syntax_hints),
-                    ontology=GAILA_PHASE_1_ONTOLOGY,
-                    chooser=PHASE1_CHOOSER,
-                    max_to_sample=num_samples,
-                )
-                for syntax_hints in syntax_hints_options
-            ]
-        ),
-    )
-
-
-def _make_fall_in(num_samples: int = 5, *, noise_objects: int = 0) -> Phase1InstanceGroup:
-    theme = standard_object("theme", THING)
-    goal_reference = standard_object(
-        "goal_reference", THING, required_properties=[HOLLOW]
-    )
-    background = immutableset(
-        standard_object(f"noise_object_{x}") for x in range(noise_objects)
-    )
-    syntax_hints_options = ([], [USE_ADVERBIAL_PATH_MODIFIER])  # type: ignore
-    return phase1_instances(
-        "Fall In",
-        flatten(
-            [
-                sampled(
-                    _fall_on_template(theme, goal_reference, background, syntax_hints),
-                    ontology=GAILA_PHASE_1_ONTOLOGY,
-                    chooser=PHASE1_CHOOSER,
-                    max_to_sample=num_samples,
-                )
-                for syntax_hints in syntax_hints_options
-            ]
-        ),
-    )
-
-
-def _make_fall_beside(
-    num_samples: int = 5, *, noise_objects: int = 0
-) -> Phase1InstanceGroup:
-    theme = standard_object("theme", THING)
-    goal_reference = standard_object("goal_reference", THING)
-    background = immutableset(
-        standard_object(f"noise_object_{x}") for x in range(noise_objects)
-    )
-    syntax_hints_options: ([], [USE_ADVERBIAL_PATH_MODIFIER])  # type: ignore
-    return phase1_instances(
-        "Fall Beside",
-        flatten(
-            [
-                sampled(
-                    _fall_beside_template(
-                        theme,
-                        goal_reference,
-                        background,
-                        syntax_hints,
-                        is_right,
-                        is_distal,
-                    ),
-                    ontology=GAILA_PHASE_1_ONTOLOGY,
-                    chooser=PHASE1_CHOOSER,
-                    max_to_sample=num_samples,
-                )
-                for syntax_hints in syntax_hints_options
-                for is_right in BOOL_SET
-                for is_distal in BOOL_SET
-            ]
-        ),
-    )
-
-
-def _make_fall_in_front_of(
-    num_samples: int = 5, *, noise_objects: int = 0
-) -> Phase1InstanceGroup:
-    theme = standard_object("theme", THING)
-    goal_reference = standard_object("goal_reference", THING)
-    background = immutableset(
-        standard_object(f"noise_object_{x}") for x in range(noise_objects)
-    )
-    syntax_hints_options = ([], [USE_ADVERBIAL_PATH_MODIFIER])  # type: ignore
-    return phase1_instances(
-        "Fall In Front Of",
-        flatten(
-            [
-                sampled(
-                    _fall_in_front_of_template(
-                        theme, goal_reference, background, syntax_hints, is_distal
-                    ),
-                    ontology=GAILA_PHASE_1_ONTOLOGY,
-                    chooser=PHASE1_CHOOSER,
-                    max_to_sample=num_samples,
-                )
-                for syntax_hints in syntax_hints_options
-                for is_distal in BOOL_SET
-            ]
-        ),
-    )
-
-
-def _make_fall_behind(
-    num_samples: int = 5, *, noise_objects: int = 0
-) -> Phase1InstanceGroup:
-    theme = standard_object("theme", THING)
-    goal_reference = standard_object("goal_reference", THING)
-    background = immutableset(
-        standard_object(f"noise_object_{x}") for x in range(noise_objects)
-    )
-    syntax_hints_options = ([], [USE_ADVERBIAL_PATH_MODIFIER])  # type: ignore
-    return phase1_instances(
-        "Fall Behind",
-        flatten(
-            [
-                sampled(
-                    _fall_behind_template(
-                        theme, goal_reference, background, syntax_hints, is_distal
-                    ),
-                    ontology=GAILA_PHASE_1_ONTOLOGY,
-                    chooser=PHASE1_CHOOSER,
-                    max_to_sample=num_samples,
-                )
-                for syntax_hints in syntax_hints_options
-                for is_distal in BOOL_SET
-            ]
+                        ontology=GAILA_PHASE_1_ONTOLOGY,
+                        chooser=PHASE1_CHOOSER,
+                        max_to_sample=num_samples,
+                    )
+                    for syntax_hints in syntax_hints_options
+                    for is_distal in BOOL_SET
+                    for is_in_front in BOOL_SET
+                ]
+            ),
         ),
     )
 
@@ -348,10 +255,4 @@ def _make_fall_behind(
 def make_verb_with_dynamic_prepositions_curriculum(
     num_samples: int = 5, *, num_noise_objects: int = 0
 ):
-    return [
-        _make_fall_on(num_samples, noise_objects=num_noise_objects),
-        _make_fall_in(num_samples, noise_objects=num_noise_objects),
-        _make_fall_beside(num_samples, noise_objects=num_noise_objects),
-        _make_fall_in_front_of(num_samples, noise_objects=num_noise_objects),
-        _make_fall_behind(num_samples, noise_objects=num_noise_objects),
-    ]
+    return [_make_fall_with_prepositions(num_samples, noise_objects=num_noise_objects)]
