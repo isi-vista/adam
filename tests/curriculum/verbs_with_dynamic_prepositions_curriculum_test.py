ce8f90814caf12aba7ab671aa98172e6c482fc15
Liz Lee <elgclee@gmail.com>
2020-02-24 16:28:37 -0500

    Add curriculum test

--- tests/curriculum/verbs_with_dynamic_prepositions_curriculum_test.py
+++ tests/curriculum/verbs_with_dynamic_prepositions_curriculum_test.py
@@ -3,6 +3,7 @@ from adam.curriculum.verbs_with_dynamic_prepositions_curriculum import (
     _make_go_with_prepositions,
     _make_roll_with_prepositions,
     _make_sit_with_prepositions,
+    _make_take_with_prepositions,
 )
 from tests.curriculum.phase1_curriculum_test import curriculum_test
 
@@ -21,3 +22,7 @@ def test_make_roll():
 
 def test_make_sit():
     curriculum_test(_make_sit_with_prepositions())
+
+
+def test_make_take():
+    curriculum_test(_make_take_with_prepositions())
