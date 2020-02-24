89b367cc8549af865604fa917aabe3a2024f4faa
Liz <elgclee@gmail.com>
2020-02-24 10:48:30 -0500

    Curriculum for "sit down" (#575)

--- adam/curriculum/verbs_with_dynamic_prepositions_curriculum.py
+++ adam/curriculum/verbs_with_dynamic_prepositions_curriculum.py
@@ -1,3 +1,5 @@
+from immutablecollections import ImmutableSet
+
 from itertools import chain
 from typing import Iterable
 
@@ -15,6 +17,9 @@ from adam.curriculum.curriculum_utils import (
     PHASE1_CHOOSER,
     Phase1InstanceGroup,
 )
+from adam.language_specific.english.english_language_generator import (
+    USE_ADVERBIAL_PATH_MODIFIER,
+)
 from adam.ontology import THING
 from adam.ontology.during import DuringAction
 from adam.ontology.phase1_ontology import (
@@ -23,14 +28,18 @@ from adam.ontology.phase1_ontology import (
     GOAL,
     GAILA_PHASE_1_ONTOLOGY,
     HOLLOW,
+    SIT,
+    ANIMATE,
+    INANIMATE_OBJECT,
+    CAN_HAVE_THINGS_RESTING_ON_THEM,
+    SIT_THING_SAT_ON,
+    CAN_BE_SAT_ON_BY_PEOPLE,
+    EXTERIOR_BUT_IN_CONTACT,
     HAS_SPACE_UNDER,
     PUSH,
     THEME,
     PUSH_SURFACE_AUX,
     on,
-    ANIMATE,
-    INANIMATE_OBJECT,
-    CAN_HAVE_THINGS_RESTING_ON_THEM,
     GO,
     _GO_GOAL,
     above,
@@ -474,6 +483,68 @@ def _go_over_under_path_template(
     )
 
 
+# SIT templates
+
+
+def _sit_on_template(
+    agent: TemplateObjectVariable,
+    surface: TemplateObjectVariable,
+    seat: TemplateObjectVariable,
+    background: Iterable[TemplateObjectVariable],
+    syntax_hints: ImmutableSet[str],
+) -> Phase1SituationTemplate:
+    return Phase1SituationTemplate(
+        f"{agent.handle}-sits-on-{seat.handle}",
+        salient_object_variables=[agent, seat],
+        background_object_variables=background,
+        actions=[
+            Action(
+                SIT,
+                argument_roles_to_fillers=[
+                    (AGENT, agent),
+                    (
+                        GOAL,
+                        Region(
+                            seat,
+                            direction=GRAVITATIONAL_UP,
+                            distance=EXTERIOR_BUT_IN_CONTACT,
+                        ),
+                    ),
+                ],
+                auxiliary_variable_bindings=[(SIT_THING_SAT_ON, seat)],
+            )
+        ],
+        constraining_relations=[bigger_than(surface, seat), bigger_than(seat, agent)],
+        syntax_hints=syntax_hints,
+    )
+
+
+def _sit_in_template(
+    agent: TemplateObjectVariable,
+    surface: TemplateObjectVariable,
+    seat: TemplateObjectVariable,
+    background: Iterable[TemplateObjectVariable],
+    syntax_hints: ImmutableSet[str],
+) -> Phase1SituationTemplate:
+    return Phase1SituationTemplate(
+        f"{agent.handle}-sits-(down)-in-{seat.handle}",
+        salient_object_variables=[agent, seat],
+        background_object_variables=background,
+        actions=[
+            Action(
+                SIT,
+                argument_roles_to_fillers=[
+                    (AGENT, agent),
+                    (GOAL, Region(seat, distance=INTERIOR)),
+                ],
+                auxiliary_variable_bindings=[(SIT_THING_SAT_ON, seat)],
+            )
+        ],
+        constraining_relations=[bigger_than(surface, seat), bigger_than(seat, agent)],
+        syntax_hints=syntax_hints,
+    )
+
+
 def _make_push_with_prepositions(
     num_samples: int = 5, *, noise_objects: int = 0
 ) -> Phase1InstanceGroup:
@@ -701,10 +772,60 @@ def _make_go_with_prepositions(num_samples: int = 5, *, noise_objects: int = 0):
     )
 
 
+def _make_sit_with_prepositions(
+    num_samples: int = 5, *, noise_objects: int = 0
+) -> Phase1InstanceGroup:
+    agent = standard_object("agent", THING, required_properties=[ANIMATE])
+    seat = standard_object(
+        "seat", INANIMATE_OBJECT, required_properties=[CAN_BE_SAT_ON_BY_PEOPLE]
+    )
+    seat_in = standard_object("seat_in", INANIMATE_OBJECT, required_properties=[HOLLOW])
+    surface = standard_object(
+        "surface", THING, required_properties=[CAN_HAVE_THINGS_RESTING_ON_THEM]
+    )
+    background = immutableset(
+        standard_object(f"noise_object_{x}") for x in range(noise_objects)
+    )
+    syntax_hints_options = ([], [USE_ADVERBIAL_PATH_MODIFIER])  # type: ignore
+
+    return phase1_instances(
+        "Sit + PP",
+        chain(
+            # on
+            flatten(
+                [
+                    sampled(
+                        _sit_on_template(agent, seat, surface, background, syntax_hints),
+                        ontology=GAILA_PHASE_1_ONTOLOGY,
+                        chooser=PHASE1_CHOOSER,
+                        max_to_sample=num_samples,
+                    )
+                    for syntax_hints in syntax_hints_options
+                ]
+            ),
+            # in
+            flatten(
+                [
+                    sampled(
+                        _sit_in_template(
+                            agent, seat_in, surface, background, syntax_hints
+                        ),
+                        ontology=GAILA_PHASE_1_ONTOLOGY,
+                        chooser=PHASE1_CHOOSER,
+                        max_to_sample=num_samples,
+                    )
+                    for syntax_hints in syntax_hints_options
+                ]
+            ),
+        ),
+    )
+
+
 def make_verb_with_dynamic_prepositions_curriculum(
     num_samples: int = 5, *, num_noise_objects: int = 0
 ):
     return [
         _make_push_with_prepositions(num_samples, noise_objects=num_noise_objects),
         _make_go_with_prepositions(num_samples, noise_objects=num_noise_objects),
+        _make_sit_with_prepositions(num_samples, noise_objects=num_noise_objects),
     ]
--- tests/curriculum/verbs_with_dynamic_prepositions_curriculum_test.py
+++ tests/curriculum/verbs_with_dynamic_prepositions_curriculum_test.py
@@ -1,6 +1,7 @@
 from adam.curriculum.verbs_with_dynamic_prepositions_curriculum import (
     _make_push_with_prepositions,
     _make_go_with_prepositions,
+    _make_sit_with_prepositions,
 )
 from tests.curriculum.phase1_curriculum_test import curriculum_test
 
@@ -11,3 +12,7 @@ def test_make_push():
 
 def test_make_go():
     curriculum_test(_make_go_with_prepositions())
+
+
+def test_make_sit():
+    curriculum_test(_make_sit_with_prepositions())
