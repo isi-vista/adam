0dcd2a91ecd5c565da9db746a5e2231dd55c2fb1
Jacob L <jalichtefeld@gmail.com>
2020-03-20 12:05:21 -0400

    Add 'my' 'your' unit tests to attribute learner

--- tests/learner/subset_attribute_learner_test.py
+++ tests/learner/subset_attribute_learner_test.py
@@ -8,9 +8,14 @@ from adam.curriculum.curriculum_utils import (
     phase1_instances,
     PHASE1_CHOOSER_FACTORY,
 )
-from adam.curriculum.phase1_curriculum import _object_with_color_template
+from adam.curriculum.phase1_curriculum import (
+    _object_with_color_template,
+    _x_has_y_template,
+)
+from adam.language_specific.english.english_language_generator import IGNORE_HAS_AS_VERB
 from adam.learner import LearningExample
 from adam.learner.attributes import SubsetAttributeLearner
+from adam.ontology import IS_SPEAKER, IS_ADDRESSEE
 from adam.ontology.phase1_ontology import (
     RED,
     WHITE,
@@ -21,6 +26,9 @@ from adam.ontology.phase1_ontology import (
     BOOK,
     GAILA_PHASE_1_ONTOLOGY,
     CAR,
+    PERSON,
+    INANIMATE_OBJECT,
+    PERSON_CAN_HAVE,
 )
 from adam.situation.templates.phase1_templates import property_variable, sampled
 from tests.learner import TEST_OBJECT_RECOGNIZER
@@ -98,6 +106,117 @@ def test_subset_color_attribute_learner(color_node, object_0_node, object_1_node
         test_lingustics_description,
         test_perceptual_representation,
     ) in color_test_curriculum.instances():
+        descriptions_from_learner = learner.describe(test_perceptual_representation)
+        gold = test_lingustics_description.as_token_sequence()
+        assert descriptions_from_learner
+        assert [desc.as_token_sequence() for desc in descriptions_from_learner][0] == gold
+
+
+def test_subset_my_attribute_learner():
+    person = standard_object("speaker", PERSON, added_properties=[IS_SPEAKER])
+    inanimate_object = standard_object(
+        "object", INANIMATE_OBJECT, required_properties=[PERSON_CAN_HAVE]
+    )
+
+    my_train_curriculum = phase1_instances(
+        "my-train",
+        situations=sampled(
+            _x_has_y_template(
+                person, inanimate_object, syntax_hints=[IGNORE_HAS_AS_VERB]
+            ),
+            ontology=GAILA_PHASE_1_ONTOLOGY,
+            chooser=PHASE1_CHOOSER_FACTORY(),
+            max_to_sample=2,
+        ),
+    )
+
+    my_test_curriculum = phase1_instances(
+        "my-test",
+        situations=sampled(
+            _x_has_y_template(
+                person, inanimate_object, syntax_hints=[IGNORE_HAS_AS_VERB]
+            ),
+            ontology=GAILA_PHASE_1_ONTOLOGY,
+            chooser=PHASE1_CHOOSER_FACTORY(),
+            max_to_sample=1,
+        ),
+    )
+
+    learner = SUBSET_ATTRIBUTE_LEARNER_FACTORY()
+
+    for (
+        _,
+        linguistic_description,
+        perceptual_representation,
+    ) in my_train_curriculum.instances():
+        learner.observe(
+            LearningExample(perceptual_representation, linguistic_description)
+        )
+
+    for (
+        _,
+        test_lingustics_description,
+        test_perceptual_representation,
+    ) in my_test_curriculum.instances():
+        descriptions_from_learner = learner.describe(test_perceptual_representation)
+        gold = test_lingustics_description.as_token_sequence()
+        assert descriptions_from_learner
+        assert [desc.as_token_sequence() for desc in descriptions_from_learner][0] == gold
+
+
+def test_your_attribute_learner():
+    person_0 = standard_object("speaker", PERSON, added_properties=[IS_SPEAKER])
+    person_1 = standard_object("addressee", PERSON, added_properties=[IS_ADDRESSEE])
+    inanimate_object = standard_object(
+        "object", INANIMATE_OBJECT, required_properties=[PERSON_CAN_HAVE]
+    )
+
+    your_train_curriculum = phase1_instances(
+        "your-train",
+        situations=sampled(
+            _x_has_y_template(
+                person_1,
+                inanimate_object,
+                background=[person_0],
+                syntax_hints=[IGNORE_HAS_AS_VERB],
+            ),
+            ontology=GAILA_PHASE_1_ONTOLOGY,
+            chooser=PHASE1_CHOOSER_FACTORY(),
+            max_to_sample=2,
+        ),
+    )
+
+    your_test_curriculum = phase1_instances(
+        "your-test",
+        situations=sampled(
+            _x_has_y_template(
+                person_1,
+                inanimate_object,
+                background=[person_0],
+                syntax_hints=[IGNORE_HAS_AS_VERB],
+            ),
+            ontology=GAILA_PHASE_1_ONTOLOGY,
+            chooser=PHASE1_CHOOSER_FACTORY(),
+            max_to_sample=1,
+        ),
+    )
+
+    learner = SUBSET_ATTRIBUTE_LEARNER_FACTORY()
+
+    for (
+        _,
+        linguistic_description,
+        perceptual_representation,
+    ) in your_train_curriculum.instances():
+        learner.observe(
+            LearningExample(perceptual_representation, linguistic_description)
+        )
+
+    for (
+        _,
+        test_lingustics_description,
+        test_perceptual_representation,
+    ) in your_test_curriculum.instances():
         descriptions_from_learner = learner.describe(test_perceptual_representation)
         gold = test_lingustics_description.as_token_sequence()
         assert descriptions_from_learner
