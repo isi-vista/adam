5b50b0949d970e6d6d8f4103d03c17b4baa49fb8
Ryan Gabbard <gabbard@isi.edu>
2020-03-20 15:16:11 -0400

    Support including/excluding parts of M9 curriculum

--- adam/experiment/run_m9.py
+++ adam/experiment/run_m9.py
@@ -1,4 +1,6 @@
 import logging
+from pathlib import Path
+from typing import List
 
 from vistautils.parameters import Parameters, YAMLParametersLoader
 from vistautils.parameters_only_entrypoint import parameters_only_entry_point
@@ -9,12 +11,19 @@ from adam.experiment.log_experiment import log_experiment_entry_point
 def main(params: Parameters):
     adam_root = params.existing_directory("adam_root")
     m9_experiments_dir = adam_root / "parameters" / "experiments" / "m9"
-    param_files = [
-        m9_experiments_dir / "objects.params",
-        m9_experiments_dir / "attributes.params",
-        m9_experiments_dir / "relations.params",
-        m9_experiments_dir / "events.params",
-    ]
+    param_files: List[Path] = []
+
+    if params.boolean("include_objects", default=True):
+        param_files.append(m9_experiments_dir / "objects.params")
+
+    if params.boolean("include_attributes", default=True):
+        param_files.append(m9_experiments_dir / "attributes.params")
+
+    if params.boolean("include_relations", default=True):
+        param_files.append(m9_experiments_dir / "relations.params")
+
+    if params.boolean("include_events", default=True):
+        param_files.append(m9_experiments_dir / "events.params")
 
     # If any of the param files don't exist, bail out earlier instead of making the user
     # wait for the error.
