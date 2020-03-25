27d83206a468d9f5000b71a317eba4d1891f6a9e
Jacob L <jalichtefeld@gmail.com>
2020-03-25 14:18:46 -0400

    fixup! Debug for "object_on_ground"

--- adam/experiment/log_experiment.py
+++ adam/experiment/log_experiment.py
@@ -20,7 +20,6 @@ from adam.curriculum.phase1_curriculum import (
     build_gaila_phase1_attribute_curriculum,
     build_gaila_phase1_relation_curriculum,
     build_gaila_phase1_verb_curriculum,
-    _make_put_on_speaker_addressee_body_part_curriculum,
     _make_object_on_ground_curriculum,
 )
 from adam.curriculum.pursuit_curriculum import make_simple_pursuit_curriculum
