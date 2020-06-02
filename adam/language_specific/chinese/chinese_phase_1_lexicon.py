3859b3b90656f5a4af8da55ddecc5bcea2fb3890
paynesa <paynesa@sas.upenn.edu>
2020-06-02 14:23:05 -0400

    additional lexical updates

--- adam/language_specific/chinese/chinese_phase_1_lexicon.py
+++ adam/language_specific/chinese/chinese_phase_1_lexicon.py
@@ -1,23 +1,31 @@
-#import universal dependencies
+'''Outstanding questions:
+handling of mass nouns in Chinese?
+can we have spaces in our Chinese transcriptions?
+should we specify plurals even though they aren't morphologically salient?'''
+
+# import universal dependencies
 from adam.language.dependency.universal_dependencies import (
     ADJECTIVE,
     NOUN,
     PROPER_NOUN,
     VERB,
 )
-#import lexicon classes
+
+# import lexicon classes
 from adam.language.lexicon import LexiconEntry, LexiconProperty
-#import OntologyLexicon class
+
+# import OntologyLexicon class
 from adam.language.ontology_dictionary import OntologyLexicon
 
-#TODO: update this when Chinese syntax file ready
+# TODO: update this when Chinese syntax file ready
 from adam.language_specific.english.english_syntax import (
     FIRST_PERSON,
     SECOND_PERSON,
     NOMINATIVE,
     ACCUSATIVE,
 )
-#import ontology items
+
+# import ontology items
 from adam.ontology.phase1_ontology import (
     BABY,
     BALL,
@@ -70,17 +78,15 @@ from adam.ontology.phase1_ontology import (
     DARK_BROWN,
 )
 
-#TODO: do we need to specify mass nouns for Chinese since most scholars say nearly all Chinese nouns are mass?
 MASS_NOUN = LexiconProperty("mass-noun")
-#ditransitive property for verbs
+# ditransitive property for verbs
 ALLOWS_DITRANSITIVE = LexiconProperty("allows-ditransitive")
 
 
-'''Chinese transcriptions use Yale romanization here since this allows us to maintain UTF-8 encoding 
-(v.s. Pinyin, Simplified Chinese, etc.)'''
+"""Chinese transcriptions use Yale romanization here since this allows us to maintain UTF-8 encoding 
+(v.s. Pinyin, Simplified Chinese, etc.)"""
 
-#TODO is it ok for there to be spaces in the strings or should they be replaced?
-#first person nominative & accusative pronoun lexicon entry
+# first person nominative & accusative pronoun lexicon entry
 I = LexiconEntry(
     "wo3",
     NOUN,
@@ -88,17 +94,15 @@ I = LexiconEntry(
     intrinsic_morphosyntactic_properties=[FIRST_PERSON],
 )
 
-#second person nominative & accusative pronoun lexicon entry
+# second person nominative & accusative pronoun lexicon entry
 YOU = LexiconEntry(
     "ni3",
     NOUN,
     plural_form="ni3 men",
-    intrinsic_morphosyntactic_properties=[SECOND_PERSON]
+    intrinsic_morphosyntactic_properties=[SECOND_PERSON],
 )
 
-#define the main lexicon
-#TODO: do plural forms need to be specified or can they be left?
-#TODO: do we want to keep the mass noun / non-mass noun distinction even though Chinese doesn't make it morphologically?
+# define the main lexicon
 GAILA_PHASE_1_CHINESE_LEXICON = OntologyLexicon(
     ontology=GAILA_PHASE_1_ONTOLOGY,
     ontology_node_to_word=(
@@ -106,8 +110,8 @@ GAILA_PHASE_1_CHINESE_LEXICON = OntologyLexicon(
         (MOM, LexiconEntry("ma1", PROPER_NOUN)),
         (BALL, LexiconEntry("chyou2", NOUN)),
         (TABLE, LexiconEntry("jwo1", NOUN)),
-        (PUT, LexiconEntry("fang4", NOUN)),
-        (PUSH, LexiconEntry("twei1", NOUN)),
+        (PUT, LexiconEntry("fang4", VERB)),
+        (PUSH, LexiconEntry("twei1", VERB)),
         (BOOK, LexiconEntry("shu1", NOUN)),
         (HOUSE, LexiconEntry("wu1", NOUN)),
         (CAR, LexiconEntry("chi4 che1", NOUN)),
@@ -118,7 +122,19 @@ GAILA_PHASE_1_CHINESE_LEXICON = OntologyLexicon(
         (CHAIR, LexiconEntry("yi3 dz", NOUN)),
         (HEAD, LexiconEntry("tou2", NOUN)),
         (MILK, LexiconEntry("nyou2 nai3", NOUN, [MASS_NOUN])),
-        (HAND, LexiconEntry("shou3", NOUN))
-    )
+        (HAND, LexiconEntry("shou3", NOUN)),
+        (TRUCK, LexiconEntry("ka3 che1", NOUN)),
+        (DOOR, LexiconEntry("men2", NOUN)),
+        (HAT, LexiconEntry("mau4 dz", NOUN)),
+        (COOKIE, LexiconEntry("chyu1 chi2 bing3", NOUN)),
+        (DAD, LexiconEntry("ba4", PROPER_NOUN)),
+        (BABY, LexiconEntry("bau3 bau3", NOUN)),
+        (DOG, LexiconEntry("gou3", NOUN)),
+        (BIRD, LexiconEntry("nyau3", NOUN)),
+        (GO, LexiconEntry("dzou3", VERB)),
+        (COME, LexiconEntry("lái", VERB)),
+        (TAKE, LexiconEntry("na2", VERB)),
+        (EAT, LexiconEntry("chr1", VERB)),
+    ),
 )
 
