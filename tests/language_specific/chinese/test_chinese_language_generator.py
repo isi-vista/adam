0ff3a1412523d5112a46469e1d9821515d136331
paynesa <paynesa@sas.upenn.edu>
2020-06-09 10:47:55 -0400

    count & many phrases (including ones that will need classifiers)

--- tests/language_specific/chinese/test_chinese_language_generator.py
+++ tests/language_specific/chinese/test_chinese_language_generator.py
@@ -88,9 +88,9 @@ _SIMPLE_GENERATOR = SimpleRuleBasedChineseLanguageGenerator(
 )
 
 
-"""NOUN PHRASE TESTS"""
+""" BASIC NOUN PHRASE TESTS"""
 
-
+# just a single common noun
 @pytest.mark.skip(reason="NPs aren't yet supported by our Chinese language generator")
 def test_common_noun():
     situation = HighLevelSemanticsSituation(
@@ -98,9 +98,10 @@ def test_common_noun():
     )
     assert only(
         _SIMPLE_GENERATOR.generate_language(situation, FixedIndexChooser(0))
-    ).as_token_sequence() == ("chyou2")
+    ).as_token_sequence() == ("chyou2",)
 
 
+# a single mass noun. The distinction isn't nearly as salient in Chinese
 @pytest.mark.skip(reason="NP's aren't yet supported by our Chinese language generator")
 def test_mass_noun():
     situtation = HighLevelSemanticsSituation(
@@ -108,4 +109,59 @@ def test_mass_noun():
     )
     assert only(
         _SIMPLE_GENERATOR.generate_language(situation, FixedIndexChooser(0))
-    ).as_token_sequence() == ("shwei3")
+    ).as_token_sequence() == ("shwei3",)
+
+
+# a single proper noun
+@pytest.mark.skip(reason="NP's aren't yet supported by our Chinese language generator")
+def test_proper_noun():
+    situation = HighLevelSemanticsSituation(
+        ontology=GAILA_PHASE_1_ONTOLOGY, salient_objects=[situation_objects[DAD]]
+    )
+    assert only(
+        _SIMPLE_GENERATOR.generate_language(situation, FixedIndexChooser(0))
+    ).as_token_sequence() == ("ba4 ba4",)
+
+
+"""COUNTING NOUN PHRASE TESTS (WITH CLASSIFIERS)"""
+
+# a single object
+# TODO: we need to mark whether we want to specify that there is one object
+# TODO: these classifiers should be checked by a native speaker
+@pytest.mark.skip(reason="Classifiers aren't yet supported")
+def test_single_item():
+    dog = situation_object(DOG)
+    situation = HighLevelSemanticsSituation(
+        ontology=GAILA_PHASE_1_ONTOLOGY, salient_objects=[dog]
+    )
+    assert only(
+        _SIMPLE_GENERATOR.generate_language(situation, FixedIndexChooser(0))
+    ).as_token_sequence() == ("yi1", "jr3", "gou3")
+
+
+# two objects, which can be counted distinctly
+@pytest.mark.skip(reason="Classifiers aren't yet supported")
+def test_two_items():
+    dog1 = situation_object(DOG, debug_handle="dog1")
+    dog2 = situation_object(DOG, debug_handle="dog2")
+    situation = HighLevelSemanticsSituation(
+        ontology=GAILA_PHASE_1_ONTOLOGY, salient_objects=[dog1, dog2]
+    )
+    assert only(
+        _SIMPLE_GENERATOR.generate_language(situation, FixedIndexChooser(0))
+    ).as_token_sequence() == ("lyang3", "jr3", "gou3")
+
+
+# many objects
+# TODO: this can be implemented before classifier issue is resolved
+@pytest.mark.skip(reason="Many object NPs aren't yet supported")
+def test_many_items():
+    ball1 = situation_object(BALL, debug_handle="ball1")
+    ball2 = situation_object(BALL, debug_handle="ball2")
+    ball3 = situation_object(BALL, debug_handle="ball3")
+    situation = HighLevelSemanticsSituation(
+        ontology=GAILA_PHASE_1_ONTOLOGY, salient_objects=[ball1, ball2, ball3]
+    )
+    assert only(
+        _SIMPLE_GENERATOR.generate_language(situation, FixedIndexChooser(0))
+    ).as_token_sequence() == ("hen3dwo1", "chyou2")
