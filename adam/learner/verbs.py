c14f6a316ae6501429f45a08b48e0c9123c303d3
Jacob Lichtefeld <jalichtefeld@gmail.com>
2020-06-09 08:31:16 -0700

    WIP of new templatic method for verb hypothesis

--- adam/learner/verbs.py
+++ adam/learner/verbs.py
@@ -1,5 +1,8 @@
+import itertools
 from abc import ABC
-from typing import AbstractSet, List, Mapping, Union
+from enum import Enum
+from typing import AbstractSet, Mapping, Union, List, Iterable, Optional, Dict, Tuple
+from pathlib import Path
 
 from adam.language import LinguisticDescription
 from adam.learner import LearningExample
@@ -36,8 +39,8 @@ from adam.perception.developmental_primitive_perception import (
 from adam.perception.perception_graph import PerceptionGraph
 from adam.semantics import ActionConcept, ObjectSemanticNode, SyntaxSemanticsVariable
 from attr import attrib, attrs
+from immutablecollections import immutabledict, immutableset, ImmutableSet
 from attr.validators import instance_of
-from immutablecollections import immutabledict, immutableset
 from vistautils.span import Span
 
 # This is the maximum number of tokens we will hypothesize
@@ -45,6 +48,17 @@ from vistautils.span import Span
 _MAXIMUM_ACTION_TEMPLATE_TOKEN_LENGTH = 3
 _LEFT = -1
 _RIGHT = 1
+
+
+class VerbAlignmentSlots(Enum):
+    Argument = "Argument"
+    FixedString = "FixedString"
+
+
+@attrs(frozen=True)
+class SemanticNodeWithSpan:
+    node: SemanticNode = attrib(validator=instance_of(SemanticNode))
+    span: Span = attrib(validator=instance_of(Span))
 
 
 @attrs
@@ -57,6 +71,9 @@ class AbstractVerbTemplateLearnerNew(AbstractTemplateLearnerNew, ABC):
         language_concept_alignment = (
             language_perception_semantic_alignment.language_concept_alignment
         )
+        num_arguments_to_alignments_sets: Dict[
+            int, ImmutableSet[List[SemanticNodeWithSpan]]
+        ] = {}
         sentence_tokens = language_concept_alignment.language.as_token_sequence()
 
         # Any recognized object is a potential verb argument.
@@ -85,6 +102,124 @@ class AbstractVerbTemplateLearnerNew(AbstractTemplateLearnerNew, ABC):
                 if language_concept_alignment.token_index_is_aligned(token_index):
                     return False
             return True
+
+        def candidate_verb_templates() -> Iterable[List[VerbAlignmentSlots]]:
+            # First let's handle only one argument - Intransitive Verbs
+            yield itertools.combinations(
+                [VerbAlignmentSlots.Argument, VerbAlignmentSlots.FixedString], 2
+            )
+            # Now we want to handle two arguments - Ditransitive Verbs
+            yield itertools.combinations(
+                [
+                    VerbAlignmentSlots.Argument,
+                    VerbAlignmentSlots.Argument,
+                    VerbAlignmentSlots.FixedString,
+                ],
+                3,
+            )
+            # Now we want to handle three arguments, which can either have one or two fixed strings
+            yield itertools.combinations(
+                [
+                    VerbAlignmentSlots.Argument,
+                    VerbAlignmentSlots.Argument,
+                    VerbAlignmentSlots.Argument,
+                    VerbAlignmentSlots.FixedString,
+                    VerbAlignmentSlots.FixedString,
+                ],
+                5,
+            )
+
+        def is_valid_aligned_object_nodes(
+            semantic_nodes: Tuple[SemanticNodeWithSpan]
+        ) -> bool:
+            previous_node = semantic_nodes[0]
+            for i in range(1, len(semantic_nodes)):
+                if not previous_node.span.precedes(semantic_nodes[i].span):
+                    return False
+                previous_node = semantic_nodes[i]
+            return True
+
+        def aligned_object_nodes(
+            num_arguments: int
+        ) -> Iterable[Tuple[SemanticNodeWithSpan]]:
+            # We guarantee the return order is in order of sentence appearance from left to right
+            if num_arguments not in num_arguments_to_alignments_sets.keys():
+                semantic_nodes_with_spans = immutableset(
+                    SemanticNodeWithSpan(node=node, span=span)
+                    for (
+                        node,
+                        span,
+                    ) in language_concept_alignment.node_to_language_span.items()
+                )
+
+                num_arguments_to_alignments_sets[num_arguments] = immutableset(
+                    ordered_semantic_nodes
+                    for ordered_semantic_nodes in itertools.product(
+                        semantic_nodes_with_spans, repeat=num_arguments
+                    )
+                    if is_valid_aligned_object_nodes(ordered_semantic_nodes)
+                )
+
+            return num_arguments_to_alignments_sets[num_arguments]
+
+        def process_aligned_objects_with_template(
+            verb_template: List[VerbAlignmentSlots],
+            aligned_nodes: Tuple[SemanticNodeWithSpan],
+        ) -> Iterable[Optional[SurfaceTemplateBoundToSemanticNodes]]:
+            aligned_node_index = 0
+            template_elements = []
+            slot_to_semantic_node = []
+
+            for token in verb_template:
+                if token == VerbAlignmentSlots.Argument:
+                    slot_semantic_variable = STANDARD_SLOT_VARIABLES[aligned_node_index]
+                    template_elements.append(slot_semantic_variable)
+                    slot_to_semantic_node.append(
+                        (slot_semantic_variable, aligned_nodes[aligned_node_index].node)
+                    )
+                    aligned_node_index += 1
+                else:
+                    # We need to handle searching before or after the aligned token
+                    # And we could generate multiple options of different lengths
+                    # between 1 and _MAXIMUM_ACTION_TEMPLATE_TOKEN_LENGTH
+                    if aligned_node_index == 0:
+                        raise NotImplementedError()
+                    elif aligned_node_index == len(aligned_nodes):
+                        raise NotImplementedError()
+                    else:
+                        candidate_verb_token_span = Span(
+                            aligned_nodes[aligned_node_index - 1].span.end,
+                            aligned_nodes[aligned_node_index].span.start,
+                        )
+                        if not is_legal_template_span(candidate_verb_token_span):
+                            yield None
+
+                        template_elements.extend(
+                            sentence_tokens[
+                                candidate_verb_token_span.start : candidate_verb_token_span.end
+                            ]
+                        )
+
+            yield SurfaceTemplateBoundToSemanticNodes(
+                surface_template=SurfaceTemplate(
+                    elements=template_elements,
+                    determiner_prefix_slots=[SLOT for (SLOT, _) in slot_to_semantic_node],
+                ),
+                slot_to_semantic_node=slot_to_semantic_node,
+            )
+
+        # Generate all the possible verb template alignments
+        for verb_template in candidate_verb_templates():
+            for aligned_nodes in aligned_object_nodes(
+                sum(1 for token in verb_template if token == VerbAlignmentSlots.Argument)
+            ):
+                # aligned_object_nodes is guaranteed to only give us alignments
+                # Which the spans go from left most to right most
+                for (
+                    surface_template_bound_to_semantic_nodes
+                ) in process_aligned_objects_with_template(verb_template, aligned_nodes):
+                    if surface_template_bound_to_semantic_nodes:
+                        ret.append(surface_template_bound_to_semantic_nodes)
 
         # First, we handle intransitive verbs.
         # Let's take the example "Bob falls over".
