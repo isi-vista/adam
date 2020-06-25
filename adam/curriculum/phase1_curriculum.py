0ff55bf4eda744cd95f2e44d7ad3ca03ddc139a8
Sarah Brogden Payne <paynesa@sas.upenn.edu>
2020-06-25 09:59:12 -0400

    Update adam/curriculum/phase1_curriculum.py
    Co-authored-by: Jacob Lichtefeld <jalichtefeld@gmail.com>

--- adam/curriculum/phase1_curriculum.py
+++ adam/curriculum/phase1_curriculum.py
@@ -1718,8 +1718,8 @@ def make_push_templates(
                         (
                             agent,
                             SpatialPath(
-                                operator=TO,
-                                reference_object=push_surface,
+                                operator=NONE,
+                                reference_object=push_goal,
                                 properties=spatial_properties,
                             ),
                         )
