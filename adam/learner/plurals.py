74c35ee6d49f659c22d81ecb539aac46715f540a
Deniz Beser <denizbeser15@gmail.com>
2020-07-20 12:22:56 -0400

    wip - plural learner for english works

--- adam/learner/plurals.py
+++ adam/learner/plurals.py
@@ -10,7 +10,7 @@ from adam.learner.alignments import (
     LanguagePerceptionSemanticAlignment,
     PerceptionSemanticAlignment,
 )
-from adam.learner.attributes import SubsetAttributeLearnerNew
+from adam.learner.attributes import SubsetAttributeLearnerNew, AbstractAttributeTemplateLearnerNew
 from adam.learner.learner_utils import candidate_templates, AlignmentSlots, \
     pattern_remove_incomplete_region_or_spatial_path
 from adam.learner.perception_graph_template import PerceptionGraphTemplate
@@ -30,99 +30,39 @@ from adam.semantics import ActionConcept, ObjectConcept, AttributeConcept
 
 _MAXIMUM_PLURAL_TEMPLATE_TOKEN_LENGTH = 5
 
-#
-# @attrs
-# class AbstractPluralTemplateLearnerNew(AbstractTemplateLearnerNew, ABC):
-#     # pylint:disable=abstract-method
-#     def _candidate_templates(
-#             self, language_perception_semantic_alignment: LanguagePerceptionSemanticAlignment
-#     ) -> AbstractSet[SurfaceTemplateBoundToSemanticNodes]:
-#         def candidate_plural_templates() -> Iterable[Tuple[AlignmentSlots, ...]]:
-#             # This function returns templates for the candidate plural templates
-#             # Terminology:
-#             # (A)rgument - Noun
-#             # (F)ixedString - A collection of str tokens which can be the plural marker or modifier
-#
-#             for i in [2, 3]:
-#                 for output in immutableset(
-#                         itertools.permutations(
-#                             [AlignmentSlots.Argument] + [AlignmentSlots.FixedString] * (i-1), i
-#                         )
-#                 ):
-#                     yield output
-#
-#         # Generate all the possible plural template alignments
-#         return candidate_templates(
-#             language_perception_semantic_alignment,
-#             _MAXIMUM_PLURAL_TEMPLATE_TOKEN_LENGTH,
-#             self._language_mode,
-#             candidate_plural_templates,
-#         )
-#
-#
-# @attrs
-# class SubsetPluralLearnerNew(AbstractTemplateSubsetLearnerNew, AbstractPluralTemplateLearnerNew):
-#     def _can_learn_from(
-#         self, language_perception_semantic_alignment: LanguagePerceptionSemanticAlignment
-#     ) -> bool:
-#         concepts = [s.concept for s in
-#                     language_perception_semantic_alignment.perception_semantic_alignment.semantic_nodes]
-#         counts = Counter(concepts)
-#         return max(counts.values()) > 1
-#
-#     def _new_concept(self, debug_string: str) -> AttributeConcept:
-#         return AttributeConcept(debug_string)
-#
-#     def _keep_hypothesis(
-#         self,
-#         *,
-#         hypothesis: PerceptionGraphTemplate,
-#         bound_surface_template: SurfaceTemplateBoundToSemanticNodes
-#     ) -> bool:
-#         num_template_arguments = len(bound_surface_template.slot_to_semantic_node)
-#         return len(hypothesis.graph_pattern) >= 2 * num_template_arguments
-#
-#     def _hypotheses_from_perception(
-#         self,
-#         learning_state: LanguagePerceptionSemanticAlignment,
-#         bound_surface_template: SurfaceTemplateBoundToSemanticNodes,
-#     ) -> AbstractSet[PerceptionGraphTemplate]:
-#         # For the subset learner, our hypothesis is the entire graph.
-#         return immutableset(
-#             [
-#                 PerceptionGraphTemplate.from_graph(
-#                     learning_state.perception_semantic_alignment.perception_graph,
-#                     template_variable_to_matched_object_node=bound_surface_template.slot_to_semantic_node,
-#                 )
-#             ]
-#         )
-#
-#     def _preprocess_scene(
-#         self, perception_semantic_alignment: PerceptionSemanticAlignment
-#     ) -> PerceptionSemanticAlignment:
-#         return perception_semantic_alignment
-#
-#     def _update_hypothesis(
-#         self,
-#         previous_pattern_hypothesis: PerceptionGraphTemplate,
-#         current_pattern_hypothesis: PerceptionGraphTemplate,
-#     ) -> Optional[PerceptionGraphTemplate]:
-#         return previous_pattern_hypothesis.intersection(
-#             current_pattern_hypothesis,
-#             ontology=self._ontology,
-#             match_mode=MatchMode.NON_OBJECT,
-#             allowed_matches=immutablesetmultidict(
-#                 [
-#                     (node2, node1)
-#                     for previous_slot, node1 in previous_pattern_hypothesis.template_variable_to_pattern_node.items()
-#                     for new_slot, node2 in current_pattern_hypothesis.template_variable_to_pattern_node.items()
-#                     if previous_slot == new_slot
-#                 ]
-#             ),
-#         )
 
 @attrs
-class SubsetPluralLearnerNew(SubsetAttributeLearnerNew):
+class AbstractPluralTemplateLearnerNew(AbstractTemplateLearnerNew, ABC):
+    # pylint:disable=abstract-method
+    def _candidate_templates(
+            self, language_perception_semantic_alignment: LanguagePerceptionSemanticAlignment
+    ) -> AbstractSet[SurfaceTemplateBoundToSemanticNodes]:
+        def candidate_plural_templates() -> Iterable[Tuple[AlignmentSlots, ...]]:
+            # This function returns templates for the candidate plural templates
+            # Terminology:
+            # (A)rgument - Noun
+            # (F)ixedString - A collection of str tokens which can be the plural marker or modifier
+
+            for i in [2, 3]:
+                for output in immutableset(
+                        itertools.permutations(
+                            [AlignmentSlots.Argument] + [AlignmentSlots.FixedString] * (i-1), i
+                        )
+                ):
+
+                    yield output
+
+        # Generate all the possible plural template alignments
+        return candidate_templates(
+            language_perception_semantic_alignment,
+            _MAXIMUM_PLURAL_TEMPLATE_TOKEN_LENGTH,
+            self._language_mode,
+            candidate_plural_templates,
+        )
+
+
+@attrs
+class SubsetPluralLearnerNew(AbstractTemplateSubsetLearnerNew, AbstractPluralTemplateLearnerNew):
     template_to_count_property = {}
 
     def _can_learn_from(
@@ -138,7 +78,6 @@ class SubsetPluralLearnerNew(SubsetAttributeLearnerNew):
     ) -> PerceptionSemanticAlignment:
         nodes = [s for s in perception_semantic_alignment.semantic_nodes]
         counts = Counter([s.concept for s in nodes])
-        print(counts)
         digraph = perception_semantic_alignment.perception_graph.copy_as_digraph()
         for node in nodes:
             count = counts[node.concept]
