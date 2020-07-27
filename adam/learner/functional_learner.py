6da6209474d5f35d6b7cd8e76c0f44159cff942c
Jacob Lichtefeld <jalichtefeld@gmail.com>
2020-07-27 12:06:39 -0700

    Add explanation of how the functional learner works

--- adam/learner/functional_learner.py
+++ adam/learner/functional_learner.py
@@ -29,6 +29,13 @@ from adam.semantics import (
 # This class needs a better name?
 @attrs(slots=True)
 class ConceptFunctionCounter:
+    """
+    A ConceptFunctionalCounter counts the number of times in which an object concept
+    is used as an argument in an action concept. This information is then used
+    when the learner encounters an unknown object concept in the same argument
+    slot as we have seen previously.
+    """
+
     _concept_to_count: Dict[ObjectConcept, int] = attrib(
         init=False, default=Factory(dict)
     )
@@ -84,10 +91,17 @@ class FunctionalLearner(TemplateLearner):
 
         self._observation_num += 1
 
+        # The functional learner is a 'semantics' learner rather than a perception learner
+        # That is, the functional learner 'learns' information from the perceptual learner's output
+        # Do to easily process over the semantics we go over these learner semantics here
         semantics = LearnerSemantics.from_nodes(
             language_perception_semantic_alignment.perception_semantic_alignment.semantic_nodes
         )
+        # Right now, the functional learner only learns from actions so for each action concept in
+        # the scene we do the following
         for semantic_node in semantics.actions:
+            # Check if this action concept is one the functional learner has sceen before
+            # If not, add an entry for it with a dictionary for the slot fillers
             if (
                 semantic_node.concept
                 not in self._concept_to_slots_to_function_counter.keys()
@@ -96,7 +110,10 @@ class FunctionalLearner(TemplateLearner):
                     (slot, ConceptFunctionCounter())
                     for slot in semantic_node.slot_fillings.keys()
                 )
+            # Then for each slot filler alignment in the scene
             for (slot, slot_filler) in semantic_node.slot_fillings.items():
+                # See if that slot is in our dictionary, it should be! Otherwise the
+                # semantic node is invalid
                 if (
                     slot
                     not in self._concept_to_slots_to_function_counter[
@@ -106,6 +123,7 @@ class FunctionalLearner(TemplateLearner):
                     raise RuntimeError(
                         f"Tried to align functional use to slot: {slot} in concept {semantic_node.concept} but {slot} didn't exist in the concept"
                     )
+                # Finally count this entry in a functional concept counter
                 self._concept_to_slots_to_function_counter[semantic_node.concept][
                     slot
                 ].add_example(slot_filler.concept)
@@ -113,10 +131,17 @@ class FunctionalLearner(TemplateLearner):
     def _enrich_common(
         self, perception_semantic_alignment: PerceptionSemanticAlignment
     ) -> PerceptionSemanticAlignment:
+        # The functional learner is a 'semantics' learner rather than a perception learner
+        # That is, the functional learner 'learns' information from the perceptual learner's output
+        # Do to easily process over the semantics we go over these learner semantics here
         semantics = LearnerSemantics.from_nodes(
             perception_semantic_alignment.semantic_nodes
         )
         list_of_matches: List[Tuple[FunctionalObjectConcept, ObjectConcept]] = []
+        # The functional learner only deals with action semantics so
+        # we go over each action semantics in the scene and if a slot filler
+        # is of an FunctionalObjectConcept type, then we add a mapping from that
+        # FunctionalObjectConcept to a known concept with our best guess concept
         for action_semantic_node in semantics.actions:
             if action_semantic_node.concept in self._concept_to_slots_to_function_counter:
                 for slot, slot_filler in action_semantic_node.slot_fillings.items():
