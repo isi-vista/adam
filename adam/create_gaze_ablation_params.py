e370a8a56604ac5973502cc43bce27c380bb6d29
paynesa <paynesa@sas.upenn.edu>
2020-08-07 17:36:34 -0400

    full writeout of param files

--- adam/create_gaze_ablation_params.py
+++ adam/create_gaze_ablation_params.py
@@ -1,6 +1,3 @@
-import logging
-from typing import Callable, Optional, Mapping, Iterable, Tuple
-import random
 from vistautils.parameters import Parameters
 from vistautils.parameters_only_entrypoint import parameters_only_entry_point
 import numpy as np
@@ -26,22 +23,34 @@ def create_gaze_ablation_entry_point(params: Parameters) -> None:
     min_num_noise_instances = params.integer("min_num_noise", default=0)
     max_num_noise_instances = params.integer("max_num_noise", default=0)
 
+    # get the number of instances in the entire curriculum
+    min_num_instances_in_curriculum = params.integer("min_instances", default=10)
+    max_num_instances_in_curriculum = params.integer("max_instances", default=20)
+
     # all possible numbers of noise instances
     for num_noise_instances in range(
         min_num_noise_instances, max_num_noise_instances + 1
     ):
-        # all possible numbers of instances
-        for num_objects_in_instance in range(min_num_objects, max_num_objects + 1):
-            # all possible accuracies
-            for prob_given in values_for_accuracy:
-                for prob_not_given in values_for_accuracy:
-                    for add_gaze in [True, False]:
-                        param_file_string = f"""
-_includes:
+        # all possible numbers of instances in the curriculum
+        for num_instances in range(
+            min_num_instances_in_curriculum, max_num_instances_in_curriculum + 1
+        ):
+            # all possible numbers of instances
+            for num_objects_in_instance in range(min_num_objects, max_num_objects + 1):
+                # all possible accuracies
+                for prob_given in values_for_accuracy:
+                    for prob_not_given in values_for_accuracy:
+                        for add_gaze in [True, False]:
+                            file_name = (
+                                f"pursuit-ablating-gaze-{num_instances}_instances-{num_noise_instances}_noise_instances-"
+                                f"{num_objects_in_instance}_objects_in_instance-{prob_given}_given-{prob_not_given}_not_given-{add_gaze}"
+                                f"_gaze"
+                            )
+                            param_file_string = f"""_includes:
    - "../../root.params"
    - "m13.params"
 
-experiment: 'pursuit-ablating-gaze-{num_noise_instances}_noise_instances-{num_objects_in_instance}_objects_in_instance-{prob_given}_given-{prob_not_given}_not_given-{add_gaze}_gaze'
+experiment: '{file_name}'
 curriculum: pursuit
 learner: pursuit-gaze
 accuracy_to_txt : True
@@ -53,13 +62,15 @@ pursuit:
    smoothing_parameter: .001
 
 pursuit-curriculum-params:
-   num_instances: 20
-   num_noise_instances: 0
-   num_objects_in_instance: 3
-   add_gaze : True
-   prob_given : 0.5
-   prob_not_given : 0.15"""
-                        print(param_file_string)
+   num_instances: {num_instances}
+   num_noise_instances: {num_noise_instances}
+   num_objects_in_instance: {num_objects_in_instance}
+   add_gaze : {add_gaze}
+   prob_given : {prob_given}
+   prob_not_given : {prob_not_given}"""
+
+                        with open(f"{parameters_dir}/{file_name}", "a") as f:
+                            f.write(param_file_string)
 
 
 if __name__ == "__main__":
