0a93dce827001340aae8b330da3bf8b096814056
paynesa <paynesa@sas.upenn.edu>
2020-08-10 14:26:54 -0400

    remove print/debug statements

--- adam/learner/object_recognizer.py
+++ adam/learner/object_recognizer.py
@@ -325,7 +325,6 @@ class ObjectRecognizer:
             num_object_nodes = candidate_object_graph.count_nodes_matching(
                 lambda node: isinstance(node, ObjectPerception)
             )
-            print(candidate_object_graph.copy_as_digraph().node)
 
             for (concept, pattern) in concepts_to_patterns.items():
                 # As an optimization, we count how many sub-object nodes
@@ -334,7 +333,6 @@ class ObjectRecognizer:
                 # and we can bail out early.
                 if num_object_nodes != self._concept_to_num_subobjects[concept]:
                     continue
-                print("matching to", concept.debug_string)
                 with Timer(factor=1000) as t:
                     matcher = pattern.matcher(
                         candidate_object_graph, match_mode=MatchMode.OBJECT
@@ -343,7 +341,6 @@ class ObjectRecognizer:
                         matcher.matches(use_lookahead_pruning=True), None
                     )
                 if pattern_match:
-                    print("MATCH SUCCESSFUL")
                     cumulative_millis_in_successful_matches_ms += t.elapsed
                     matched_object_node = ObjectSemanticNode(concept)
 
