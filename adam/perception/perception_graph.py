28d25bb1d8602aa4e4df17fff7832d5b42a80b0d
Joseph Cecil <jcecil@isi.edu>
2020-09-15 17:16:03 -0400

    Fix logging error

--- adam/perception/perception_graph.py
+++ adam/perception/perception_graph.py
@@ -1594,7 +1594,7 @@ class PatternMatching:
                     seen_nodes = list(self._seen_set)
                     first_saw_node_at_index = seen_nodes.index(node)
                     logging.debug(
-                        "Detected cycle of nodes %o", seen_nodes[first_saw_node_at_index:]
+                        f"Detected cycle of nodes %s", seen_nodes[first_saw_node_at_index:]
                     )
                     return True
                 else:
