0fb69e4b2189f3509ea2f6a13890d1376321fd48
paynesa <paynesa@sas.upenn.edu>
2020-12-17 23:06:33 -0500

    bilingual sequential; need to shuffle

--- adam/learner/language_mode.py
+++ adam/learner/language_mode.py
@@ -7,8 +7,13 @@ from immutablecollections import immutabledict
 class LanguageMode(Enum):
     ENGLISH = auto()
     CHINESE = auto()
+    BILINGUAL = auto()
 
 
 LANGUAGE_MODE_TO_NAME: Mapping[LanguageMode, str] = immutabledict(
-    {LanguageMode.ENGLISH: "english", LanguageMode.CHINESE: "chinese"}
+    {
+        LanguageMode.ENGLISH: "english",
+        LanguageMode.CHINESE: "chinese",
+        LanguageMode.BILINGUAL: "bilingual",
+    }
 )
