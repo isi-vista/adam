1011c0196fd7eff1242a1349a91873468e5cdeb6
gayatri1205 <gayatri.atale@gmail.com>
2021-11-19 12:20:32 -0800

    initial image slider

--- angular-viewer/adam-angular-demo/src/app/app.module.ts
+++ angular-viewer/adam-angular-demo/src/app/app.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
-// import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { SelectorsComponent } from './components/selectors/selectors.component';
 import { SelectorParentComponent } from './components/selector-parent/selector-parent.component';
 import { ButtonComponent } from './components/button/button.component';
@@ -21,12 +21,7 @@ import { HttpClientModule } from '@angular/common/http';
     ImageOutputComponent,
     ObjectResultsComponent,
   ],
-  imports: [
-    FormsModule,
-    BrowserModule,
-    // NgbModule
-    HttpClientModule,
-  ],
+  imports: [FormsModule, BrowserModule, NgbModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
 })
