f5d9f367a25264ac54b7ff6a8dd4c4d51899ca4a
gayatri1205 <gayatri.atale@gmail.com>
2021-11-23 14:33:03 -0800

    created starter panel component

---
+++ angular-viewer/adam-angular-demo/src/app/components/panel-viewer/panel-viewer.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { PanelViewerComponent } from './panel-viewer.component';
+
+describe('PanelViewerComponent', () => {
+  let component: PanelViewerComponent;
+  let fixture: ComponentFixture<PanelViewerComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ PanelViewerComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(PanelViewerComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ angular-viewer/adam-angular-demo/src/app/components/panel-viewer/panel-viewer.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-panel-viewer',
+  templateUrl: './panel-viewer.component.html',
+  styleUrls: ['./panel-viewer.component.css']
+})
+export class PanelViewerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
