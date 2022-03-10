d8b71cb6dfa22f1225a170ec86934632b198c598
Joe Cecil <jcecil@isi.edu>
2022-03-10 16:01:13 -0500

    Define stub for contrastive object learner

--- adam/learner/contrastive_learner.py
+++ adam/learner/contrastive_learner.py
@@ -1,9 +1,11 @@
 from pathlib import Path
 from typing import Protocol, NamedTuple
 
-from attr import attrs
+from attr import attrs, attrib
+from attr.validators import instance_of
 
-from adam.learner import LanguagePerceptionSemanticAlignment
+from adam.learner import ApprenticeLearner, LanguagePerceptionSemanticAlignment
+from adam.learner.objects import SubsetObjectLearner
 
 
 @attrs
@@ -37,3 +39,28 @@ class ContrastiveLearner(Protocol):
         """
         Log some representation of the contrastive learner's hypotheses to the given log directory.
         """
+
+
+@attrs
+class ContrastiveObjectLearner(Protocol):
+    """
+    A learner that learns contrasts between objects.
+
+    This learner revolves around an apprentice. This learner expects to learn from the contrasting
+    alignments produced by its apprenticed object learner. It learns in the sense that it pushes its
+    understanding down into the apprentice.
+    """
+
+    apprentice: ApprenticeLearner = attrib(validator=instance_of(SubsetObjectLearner))
+
+    def learn_from(self, matching: LanguagePerceptionSemanticContrast) -> None:
+        """
+        Learn from the given pair of semantically-aligned inputs.
+        """
+        raise NotImplementedError()
+
+    def log_hypotheses(self, log_output_path: Path) -> None:
+        """
+        Log some representation of the contrastive learner's hypotheses to the given log directory.
+        """
+        raise NotImplementedError()
