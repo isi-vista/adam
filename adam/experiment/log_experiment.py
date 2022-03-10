84e6693c57090a8c871eabf9d6ebb137e25ba39d
Joe Cecil <jcecil@isi.edu>
2022-03-10 15:41:30 -0500

    Factor out experiment_from_params()

--- adam/experiment/log_experiment.py
+++ adam/experiment/log_experiment.py
@@ -95,16 +95,6 @@ SIMULATED = "simulated"
 
 
 def log_experiment_entry_point(params: Parameters) -> None:
-    experiment_name = params.string("experiment")
-    debug_log_dir = params.optional_creatable_directory("debug_log_directory")
-
-    graph_logger: Optional[HypothesisLogger]
-    if debug_log_dir:
-        logging.info("Debug graphs will be written to %s", debug_log_dir)
-        graph_logger = HypothesisLogger(debug_log_dir, enable_graph_rendering=True)
-    else:
-        graph_logger = None
-
     debug_perception_log_dir = params.optional_creatable_directory(
         "debug_perception_log_dir"
     )
@@ -118,6 +108,36 @@ def log_experiment_entry_point(params: Parameters) -> None:
         )
     else:
         perception_graph_logger = None
+
+    execute_experiment(
+        experiment=experiment_from_params(params),
+        log_path=params.optional_creatable_directory("hypothesis_log_dir"),
+        log_hypotheses_every_n_examples=params.integer(
+            "log_hypothesis_every_n_steps", default=250
+        ),
+        log_learner_state=params.boolean("log_learner_state", default=True),
+        learner_logging_path=params.optional_creatable_directory("experiment_group_dir"),
+        starting_point=params.integer("starting_point", default=0),
+        point_to_log=params.integer("point_to_log", default=0),
+        load_learner_state=params.optional_existing_file("learner_state_path"),
+        resume_from_latest_logged_state=params.boolean(
+            "resume_from_latest_logged_state", default=False
+        ),
+        debug_learner_pickling=params.boolean("debug_learner_pickling", default=False),
+        perception_graph_logger=perception_graph_logger,
+    )
+
+
+def experiment_from_params(params: Parameters) -> Experiment:
+    experiment_name = params.string("experiment")
+    debug_log_dir = params.optional_creatable_directory("debug_log_directory")
+
+    graph_logger: Optional[HypothesisLogger]
+    if debug_log_dir:
+        logging.info("Debug graphs will be written to %s", debug_log_dir)
+        graph_logger = HypothesisLogger(debug_log_dir, enable_graph_rendering=True)
+    else:
+        graph_logger = None
 
     language_mode = params.enum(
         "language_mode", LanguageMode, default=LanguageMode.ENGLISH
@@ -272,7 +292,7 @@ def log_experiment_entry_point(params: Parameters) -> None:
         if yaml_observer_test:
             test_observer.append(yaml_observer_test)
 
-    execute_experiment(
+    return (
         Experiment(
             name=experiment_name,
             training_stages=training_instance_groups,
@@ -287,18 +307,6 @@ def log_experiment_entry_point(params: Parameters) -> None:
                 params.integer("sequence_chooser_seed", default=0)
             ),
         ),
-        log_path=params.optional_creatable_directory("hypothesis_log_dir"),
-        log_hypotheses_every_n_examples=params.integer(
-            "log_hypothesis_every_n_steps", default=250
-        ),
-        log_learner_state=params.boolean("log_learner_state", default=True),
-        learner_logging_path=experiment_group_dir,
-        starting_point=params.integer("starting_point", default=0),
-        point_to_log=params.integer("point_to_log", default=0),
-        load_learner_state=params.optional_existing_file("learner_state_path"),
-        resume_from_latest_logged_state=resume_from_last_logged_state,
-        debug_learner_pickling=params.boolean("debug_learner_pickling", default=False),
-        perception_graph_logger=perception_graph_logger,
     )
 
 
