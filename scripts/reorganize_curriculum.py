bbf3e58e732e498f13f49a5b186f6c4e51954d22
Joe Cecil <jcecil@isi.edu>
2022-05-05 15:27:09 -0400

    Rename example number variable

--- scripts/reorganize_curriculum.py
+++ scripts/reorganize_curriculum.py
@@ -47,26 +47,26 @@ def main():
     for object_name, range_examples in zip(OBJECTS_LIST, itt.repeat(N_EXAMPLES_PER_OBJECT)):
         input_curriculum_dir: Path = args.input_cur_dir / object_name
         input_feature_dir: Path = args.input_feature_dir / object_name
-        for num in range(1, range_examples):
+        for ex in range(1, range_examples):
             output_situation = output_dir / f"situation_{situation_num}"
             output_situation.mkdir(parents=True)
             # Depth Files
-            for idx, file in enumerate(sorted(input_curriculum_dir.glob(f"depth__{num}_*"))):
+            for idx, file in enumerate(sorted(input_curriculum_dir.glob(f"depth__{ex}_*"))):
                 shutil.copy(file, output_situation / f"depth_{idx}.png")
             # PDC Files
-            for idx, file in enumerate(sorted(input_curriculum_dir.glob(f"pdc_rgb__{num}_*"))):
+            for idx, file in enumerate(sorted(input_curriculum_dir.glob(f"pdc_rgb__{ex}_*"))):
                 shutil.copy(file, output_situation / f"pdc_rgb_{idx}.ply")
             # RGB Files
-            for idx, file in enumerate(sorted(input_curriculum_dir.glob(f"rgb__{num}_*"))):
+            for idx, file in enumerate(sorted(input_curriculum_dir.glob(f"rgb__{ex}_*"))):
                 shutil.copy(file, output_situation / f"rgb_{idx}.png")
             # PDC Semantic Files
-            for idx, file in enumerate(sorted(input_curriculum_dir.glob(f"pdc_semantic__{num}_*"))):
+            for idx, file in enumerate(sorted(input_curriculum_dir.glob(f"pdc_semantic__{ex}_*"))):
                 shutil.copy(file, output_situation / f"pdc_semantic_{idx}.ply")
             # Semantic Files
-            for idx, file in enumerate(sorted(input_curriculum_dir.glob(f"semantic__{num}_*"))):
+            for idx, file in enumerate(sorted(input_curriculum_dir.glob(f"semantic__{ex}_*"))):
                 shutil.copy(file, output_situation / f"semantic_{idx}.png")
             # Feature File
-            for file in sorted(input_feature_dir.glob(f"feature_{num}*")):
+            for file in sorted(input_feature_dir.glob(f"feature_{ex}*")):
                 with open(file, encoding="utf-8") as feature_file:
                     feature_yaml = yaml.safe_load(feature_file)
 
@@ -77,10 +77,10 @@ def main():
                 with open(output_situation / "feature.yaml", "w", encoding="utf-8") as feature_file:
                     yaml.dump(output_dict, feature_file)
             # Stroke File
-            for idx, file in enumerate(sorted(input_feature_dir.glob(f"stroke_{num}_*"))):
+            for idx, file in enumerate(sorted(input_feature_dir.glob(f"stroke_{ex}_*"))):
                 shutil.copy(file, output_situation / f"stroke_{idx}_{idx}.png")
             # Stroke Graph File
-            for idx, file in enumerate(sorted(input_feature_dir.glob(f"stroke_graph_{num}_*"))):
+            for idx, file in enumerate(sorted(input_feature_dir.glob(f"stroke_graph_{ex}_*"))):
                 shutil.copy(file, output_situation / f"stroke_graph_{idx}.png")
             # Description file
             with open(output_situation / "description.yaml", "w", encoding="utf-8") as description_file:
