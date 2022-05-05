c572548c4a2cdf7cb9f3738208fcc9b39f7d5204
Joe Cecil <jcecil@isi.edu>
2022-05-05 15:36:35 -0400

    Loop over camera number as well

--- scripts/reorganize_curriculum.py
+++ scripts/reorganize_curriculum.py
@@ -50,46 +50,47 @@ def main():
     ):
         input_curriculum_dir: Path = args.input_cur_dir / object_name
         input_feature_dir: Path = args.input_feature_dir / object_name
-        for ex in range(range_examples):
-            output_situation = output_dir / f"situation_{situation_num}"
-            output_situation.mkdir(parents=True)
-            # Depth Files
-            for idx, file in enumerate(sorted(input_curriculum_dir.glob(f"depth__{ex}_*"))):
-                shutil.copy(file, output_situation / f"depth_{idx}.png")
-            # PDC Files
-            for idx, file in enumerate(sorted(input_curriculum_dir.glob(f"pdc_rgb__{ex}_*"))):
-                shutil.copy(file, output_situation / f"pdc_rgb_{idx}.ply")
-            # RGB Files
-            for idx, file in enumerate(sorted(input_curriculum_dir.glob(f"rgb__{ex}_*"))):
-                shutil.copy(file, output_situation / f"rgb_{idx}.png")
-            # PDC Semantic Files
-            for idx, file in enumerate(sorted(input_curriculum_dir.glob(f"pdc_semantic__{ex}_*"))):
-                shutil.copy(file, output_situation / f"pdc_semantic_{idx}.ply")
-            # Semantic Files
-            for idx, file in enumerate(sorted(input_curriculum_dir.glob(f"semantic__{ex}_*"))):
-                shutil.copy(file, output_situation / f"semantic_{idx}.png")
-            # Feature File
-            for file in sorted(input_feature_dir.glob(f"feature_{ex}*")):
-                with open(file, encoding="utf-8") as feature_file:
-                    feature_yaml = yaml.safe_load(feature_file)
+        for cam in range(n_cameras):
+            for ex in range(range_examples):
+                output_situation = output_dir / f"situation_{situation_num}"
+                output_situation.mkdir(parents=True)
+                # Depth Files
+                for idx, file in enumerate(sorted(input_curriculum_dir.glob(f"depth__{ex}_*"))):
+                    shutil.copy(file, output_situation / f"depth_{idx}.png")
+                # PDC Files
+                for idx, file in enumerate(sorted(input_curriculum_dir.glob(f"pdc_rgb__{ex}_*"))):
+                    shutil.copy(file, output_situation / f"pdc_rgb_{idx}.ply")
+                # RGB Files
+                for idx, file in enumerate(sorted(input_curriculum_dir.glob(f"rgb__{ex}_*"))):
+                    shutil.copy(file, output_situation / f"rgb_{idx}.png")
+                # PDC Semantic Files
+                for idx, file in enumerate(sorted(input_curriculum_dir.glob(f"pdc_semantic__{ex}_*"))):
+                    shutil.copy(file, output_situation / f"pdc_semantic_{idx}.ply")
+                # Semantic Files
+                for idx, file in enumerate(sorted(input_curriculum_dir.glob(f"semantic__{ex}_*"))):
+                    shutil.copy(file, output_situation / f"semantic_{idx}.png")
+                # Feature File
+                for file in sorted(input_feature_dir.glob(f"feature_{ex}*")):
+                    with open(file, encoding="utf-8") as feature_file:
+                        feature_yaml = yaml.safe_load(feature_file)
 
-                output_dict = {
-                    "objects": [object_ for object_ in feature_yaml]
-                }
+                    output_dict = {
+                        "objects": [object_ for object_ in feature_yaml]
+                    }
 
-                with open(output_situation / "feature.yaml", "w", encoding="utf-8") as feature_file:
-                    yaml.dump(output_dict, feature_file)
-            # Stroke File
-            for idx, file in enumerate(sorted(input_feature_dir.glob(f"stroke_{ex}_*"))):
-                shutil.copy(file, output_situation / f"stroke_{idx}_{idx}.png")
-            # Stroke Graph File
-            for idx, file in enumerate(sorted(input_feature_dir.glob(f"stroke_graph_{ex}_*"))):
-                shutil.copy(file, output_situation / f"stroke_graph_{idx}.png")
-            # Description file
-            with open(output_situation / "description.yaml", "w", encoding="utf-8") as description_file:
-                yaml.dump({"language": f"a {object_name}"}, description_file)
+                    with open(output_situation / "feature.yaml", "w", encoding="utf-8") as feature_file:
+                        yaml.dump(output_dict, feature_file)
+                # Stroke File
+                for idx, file in enumerate(sorted(input_feature_dir.glob(f"stroke_{ex}_*"))):
+                    shutil.copy(file, output_situation / f"stroke_{idx}_{idx}.png")
+                # Stroke Graph File
+                for idx, file in enumerate(sorted(input_feature_dir.glob(f"stroke_graph_{ex}_*"))):
+                    shutil.copy(file, output_situation / f"stroke_graph_{idx}.png")
+                # Description file
+                with open(output_situation / "description.yaml", "w", encoding="utf-8") as description_file:
+                    yaml.dump({"language": f"a {object_name}"}, description_file)
 
-            situation_num += 1
+                situation_num += 1
 
     with open(output_dir / "info.yaml", "w", encoding="utf-8") as info_file:
         yaml.dump({"curriculum": output_dir.stem, "num_dirs": situation_num}, info_file)
