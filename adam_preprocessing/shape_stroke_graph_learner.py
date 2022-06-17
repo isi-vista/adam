67e5a9886a57ce1a6e356ce8e20286b74db2b227
Joe Cecil <jcecil@isi.edu>
2022-06-17 10:04:42 -0400

    Rename variables for clarity
    Also fix wrapping after renames.

--- adam_preprocessing/shape_stroke_graph_learner.py
+++ adam_preprocessing/shape_stroke_graph_learner.py
@@ -19,7 +19,7 @@ from MPNN import MPNN, MPNN_Linear
 from utils import accuracy, LinearModel, load_data
 from shape_stroke_extraction import Stroke_Extraction
 
-label_name = [
+string_labels = [
     'triangleblock',
     'banana',
     'book',
@@ -42,7 +42,7 @@ label_name = [
 
 ]
 
-phase = ['train', 'test']
+phases = ['train', 'test']
 
 
 
@@ -70,8 +70,8 @@ if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO)
     "Processing data from image to stroke graph"
     base_path = args.curriculum_path
-    for i in phase:
-        if i == 'train':
+    for phase in phases:
+        if phase == 'train':
             num_sample = 10
             train_coords = []
             train_adj = []
@@ -81,16 +81,16 @@ if __name__ == "__main__":
             test_coords = []
             test_adj = []
             test_label = []
-        for l, j in enumerate(label_name):
-            for p in range(1):
-                for q in range(num_sample):
+        for integer_label, string_label in enumerate(string_labels):
+            for object_view in range(1):
+                for sample_id in range(num_sample):
                     if not os.path.exists(
-                            os.path.join(base_path, 'feature', 'feature_{}_{}_{}_{}.yaml'.format(i, j, p, q))):
-                        Extractor = Stroke_Extraction(obj_type="{}_{}".format(i, j), obj_id=q, obj_view=p,
+                            os.path.join(base_path, 'feature', 'feature_{}_{}_{}_{}.yaml'.format(phase, string_label, object_view, sample_id))):
+                        Extractor = Stroke_Extraction(obj_type="{}_{}".format(phase, string_label), obj_id=sample_id, obj_view=object_view,
                                                       base_path=base_path, vis=True, save_output=True)
                         out = Extractor.get_strokes()
                     else:
-                        with open(os.path.join(base_path, 'feature', 'feature_{}_{}_{}_{}.yaml'.format(i, j, p, q)),
+                        with open(os.path.join(base_path, 'feature', 'feature_{}_{}_{}_{}.yaml'.format(phase, string_label, object_view, sample_id)),
                                   'r') as stream:
                             out = yaml.safe_load(stream)
                     num_obj = len(out)
@@ -99,23 +99,23 @@ if __name__ == "__main__":
                     coords = []
                     adj = []
                     count = [0]
-                    for ii in range(num_obj):
-                        coords.append(out[ii]['stroke_graph']['strokes_normalized_coordinates'])
-                        adj.append(out[ii]['stroke_graph']['adjacency_matrix'])
-                        count.append(len(out[ii]['stroke_graph']['adjacency_matrix']) + count[-1])
+                    for object_number in range(num_obj):
+                        coords.append(out[object_number]['stroke_graph']['strokes_normalized_coordinates'])
+                        adj.append(out[object_number]['stroke_graph']['adjacency_matrix'])
+                        count.append(len(out[object_number]['stroke_graph']['adjacency_matrix']) + count[-1])
                     coords = np.asarray(coords)
                     coords = np.concatenate(coords, 0)
                     real_adj = np.zeros([count[-1], count[-1]])
-                    for iii in range(num_obj):
-                        real_adj[count[iii]:count[iii + 1], count[iii]:count[iii + 1]] = adj[iii]
-                    if i == 'train':
+                    for object_number in range(num_obj):
+                        real_adj[count[object_number]:count[object_number + 1], count[object_number]:count[object_number + 1]] = adj[object_number]
+                    if phase == 'train':
                         train_coords.append(coords)
                         train_adj.append(real_adj)
-                        train_label.append(l)
+                        train_label.append(integer_label)
                     else:
                         test_coords.append(coords)
                         test_adj.append(real_adj)
-                        test_label.append(l)
+                        test_label.append(integer_label)
 
     device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
     # data = from_pickle('./data.pkl')
@@ -128,37 +128,61 @@ if __name__ == "__main__":
 
     "Converting stroke graph data for graph node/edge. "
     nodes, edges, num_nodes = load_data(train_coords, train_adj)
-    h = np.zeros([len(train_coords), num_nodes, 100])
-    g = np.zeros([len(train_coords), num_nodes, num_nodes])
-    e = np.zeros([len(train_coords), num_nodes, num_nodes, 100])
+    train_node_features = np.zeros([len(train_coords), num_nodes, 100])
+    train_adjacency_matrices = np.zeros([len(train_coords), num_nodes, num_nodes])
+    train_edge_features = np.zeros([len(train_coords), num_nodes, num_nodes, 100])
 
-    for i in range(len(train_coords)):
-        num_node = len(train_adj[i])
-        h[i, :num_node, :] = nodes[i]
-        g[i, :num_node, :num_node] = np.array(train_adj[i])
-        for edge in edges[i].keys():
-            e[i, edge[0], edge[1], :] = edges[i][edge]
-            e[i, edge[1], edge[0], :] = edges[i][edge]
-    h = torch.tensor(h).type(torch.FloatTensor).to(device)
-    e = torch.tensor(e).type(torch.FloatTensor).to(device)
-    g = torch.tensor(g).type(torch.FloatTensor).to(device)
+    for sample_index in range(len(train_coords)):
+        num_node = len(train_adj[sample_index])
+        train_node_features[sample_index, :num_node, :] = nodes[sample_index]
+        train_adjacency_matrices[sample_index, :num_node, :num_node] = np.array(
+            train_adj[sample_index]
+        )
+        for edge in edges[sample_index].keys():
+            train_edge_features[sample_index, edge[0], edge[1], :] = edges[sample_index][
+                edge
+            ]
+            train_edge_features[sample_index, edge[1], edge[0], :] = edges[sample_index][
+                edge
+            ]
+    train_node_features = (
+        torch.tensor(train_node_features).type(torch.FloatTensor).to(device)
+    )
+    train_edge_features = (
+        torch.tensor(train_edge_features).type(torch.FloatTensor).to(device)
+    )
+    train_adjacency_matrices = (
+        torch.tensor(train_adjacency_matrices).type(torch.FloatTensor).to(device)
+    )
     label = torch.tensor(np.array(train_label)).type(torch.LongTensor).to(device)
 
     nodes_t, edges_t, num_nodes_t = load_data(test_coords, test_adj)
-    test_h = np.zeros([len(test_coords), num_nodes_t, 100])
-    test_g = np.zeros([len(test_coords), num_nodes_t, num_nodes_t])
-    test_e = np.zeros([len(test_coords), num_nodes_t, num_nodes_t, 100])
+    test_node_features = np.zeros([len(test_coords), num_nodes_t, 100])
+    test_adjacency_matrices = np.zeros([len(test_coords), num_nodes_t, num_nodes_t])
+    test_edge_features = np.zeros([len(test_coords), num_nodes_t, num_nodes_t, 100])
 
-    for i in range(len(test_coords)):
-        num_node = len(test_adj[i])
-        test_h[i, :num_node, :] = nodes_t[i]
-        test_g[i, :num_node, :num_node] = np.array(test_adj[i])
-        for edge in edges_t[i].keys():
-            test_e[i, edge[0], edge[1], :] = edges_t[i][edge]
-            test_e[i, edge[1], edge[0], :] = edges_t[i][edge]
-    test_h = torch.tensor(test_h).type(torch.FloatTensor).to(device)
-    test_e = torch.tensor(test_e).type(torch.FloatTensor).to(device)
-    test_g = torch.tensor(test_g).type(torch.FloatTensor).to(device)
+    for sample_index in range(len(test_coords)):
+        num_node = len(test_adj[sample_index])
+        test_node_features[sample_index, :num_node, :] = nodes_t[sample_index]
+        test_adjacency_matrices[sample_index, :num_node, :num_node] = np.array(
+            test_adj[sample_index]
+        )
+        for edge in edges_t[sample_index].keys():
+            test_edge_features[sample_index, edge[0], edge[1], :] = edges_t[sample_index][
+                edge
+            ]
+            test_edge_features[sample_index, edge[1], edge[0], :] = edges_t[sample_index][
+                edge
+            ]
+    test_node_features = (
+        torch.tensor(test_node_features).type(torch.FloatTensor).to(device)
+    )
+    test_edge_features = (
+        torch.tensor(test_edge_features).type(torch.FloatTensor).to(device)
+    )
+    test_adjacency_matrices = (
+        torch.tensor(test_adjacency_matrices).type(torch.FloatTensor).to(device)
+    )
     test_label = torch.tensor(np.array(test_label)).type(torch.LongTensor).to(device)
 
     "MPNN model"
@@ -166,7 +190,7 @@ if __name__ == "__main__":
     model_base = MPNN([100, 100], hidden_state_size=100,
                       message_size=20, n_layers=1, l_target=50,
                       type='regression')
-    model = LinearModel(model_base, 50, len(label_name)).to(device)
+    model = LinearModel(model_base, 50, len(string_labels)).to(device)
     criterion = nn.NLLLoss()
     evaluation = accuracy
     optimizer = optim.Adam(model.parameters(), lr=0.001)
@@ -175,23 +199,40 @@ if __name__ == "__main__":
     best_acc = 0
 
     "Training"
-    for i in range(args.num_train_steps):
-        train_g, train_h, train_e, train_target = Variable(g), Variable(h), Variable(e), Variable(
-            label)
+    for step in range(args.num_train_steps):
+        train_g, train_h, train_e, train_target = (
+            Variable(train_adjacency_matrices),
+            Variable(train_node_features),
+            Variable(train_edge_features),
+            Variable(label),
+        )
         optimizer.zero_grad()
         output = model(train_g, train_h, train_e)
         train_loss = criterion(nn.LogSoftmax()(output), train_target)
         losses.append(train_loss.item())
         acc = Variable(evaluation(output.data, train_target.data, topk=(1,))[0])
         test_acc = Variable(
-            evaluation((nn.LogSoftmax()(model(test_g, test_h, test_e))).data, test_label.data, topk=(1,))[0])
+            evaluation(
+                (
+                    nn.LogSoftmax()(
+                        model(
+                            test_adjacency_matrices,
+                            test_node_features,
+                            test_edge_features,
+                        )
+                    )
+                ).data,
+                test_label.data,
+                topk=(1,),
+            )[0]
+        )
         if test_acc > best_acc:
             best_acc = test_acc
         train_loss.backward()
         optimizer.step()
         logging.info(
             "{}, loss: {:.4e}, acc: {}, test acc :{}".format(
-                i, train_loss.item(), acc, test_acc
+                step, train_loss.item(), acc, test_acc
             ),
             flush=True,
         )
