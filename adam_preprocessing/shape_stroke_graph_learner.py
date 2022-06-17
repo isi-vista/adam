f813698ec0671eb4c572a70336ae5c477db169a2
Joe Cecil <jcecil@isi.edu>
2022-06-17 10:37:50 -0400

    Place __main__ code into main() function

--- adam_preprocessing/shape_stroke_graph_learner.py
+++ adam_preprocessing/shape_stroke_graph_learner.py
@@ -45,9 +45,7 @@ string_labels = [
 phases = ['train', 'test']
 
 
-
-
-if __name__ == "__main__":
+def main():
     parser = ArgumentParser(description=__doc__)
     parser.add_argument(
         "curriculum_path",
@@ -85,13 +83,23 @@ if __name__ == "__main__":
             for object_view in range(1):
                 for sample_id in range(num_sample):
                     if not os.path.exists(
-                            os.path.join(base_path, 'feature', 'feature_{}_{}_{}_{}.yaml'.format(phase, string_label, object_view, sample_id))):
-                        Extractor = Stroke_Extraction(obj_type="{}_{}".format(phase, string_label), obj_id=sample_id, obj_view=object_view,
-                                                      base_path=base_path, vis=True, save_output=True)
+                        os.path.join(
+                            base_path, 'feature',
+                            'feature_{}_{}_{}_{}.yaml'.format(phase, string_label, object_view, sample_id)
+                            )
+                    ):
+                        Extractor = Stroke_Extraction(
+                            obj_type="{}_{}".format(phase, string_label), obj_id=sample_id, obj_view=object_view,
+                            base_path=base_path, vis=True, save_output=True
+                            )
                         out = Extractor.get_strokes()
                     else:
-                        with open(os.path.join(base_path, 'feature', 'feature_{}_{}_{}_{}.yaml'.format(phase, string_label, object_view, sample_id)),
-                                  'r') as stream:
+                        with open(
+                            os.path.join(
+                                base_path, 'feature',
+                                'feature_{}_{}_{}_{}.yaml'.format(phase, string_label, object_view, sample_id)
+                                ), 'r'
+                            ) as stream:
                             out = yaml.safe_load(stream)
                     num_obj = len(out)
                     if num_obj == 0:
@@ -107,7 +115,8 @@ if __name__ == "__main__":
                     coords = np.concatenate(coords, 0)
                     real_adj = np.zeros([count[-1], count[-1]])
                     for object_number in range(num_obj):
-                        real_adj[count[object_number]:count[object_number + 1], count[object_number]:count[object_number + 1]] = adj[object_number]
+                        real_adj[count[object_number]:count[object_number + 1],
+                        count[object_number]:count[object_number + 1]] = adj[object_number]
                     if phase == 'train':
                         train_coords.append(coords)
                         train_adj.append(real_adj)
@@ -116,7 +125,6 @@ if __name__ == "__main__":
                         test_coords.append(coords)
                         test_adj.append(real_adj)
                         test_label.append(integer_label)
-
     device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
     # data = from_pickle('./data.pkl')
     # train_coords = data['train_coords']
@@ -125,13 +133,11 @@ if __name__ == "__main__":
     # test_coords = data['test_coords']
     # test_adj = data['test_adj']
     # test_label = data['test_label']
-
     "Converting stroke graph data for graph node/edge. "
     nodes, edges, num_nodes = load_data(train_coords, train_adj)
     train_node_features = np.zeros([len(train_coords), num_nodes, 100])
     train_adjacency_matrices = np.zeros([len(train_coords), num_nodes, num_nodes])
     train_edge_features = np.zeros([len(train_coords), num_nodes, num_nodes, 100])
-
     for sample_index in range(len(train_coords)):
         num_node = len(train_adj[sample_index])
         train_node_features[sample_index, :num_node, :] = nodes[sample_index]
@@ -155,12 +161,10 @@ if __name__ == "__main__":
         torch.tensor(train_adjacency_matrices).type(torch.FloatTensor).to(device)
     )
     label = torch.tensor(np.array(train_label)).type(torch.LongTensor).to(device)
-
     nodes_t, edges_t, num_nodes_t = load_data(test_coords, test_adj)
     test_node_features = np.zeros([len(test_coords), num_nodes_t, 100])
     test_adjacency_matrices = np.zeros([len(test_coords), num_nodes_t, num_nodes_t])
     test_edge_features = np.zeros([len(test_coords), num_nodes_t, num_nodes_t, 100])
-
     for sample_index in range(len(test_coords)):
         num_node = len(test_adj[sample_index])
         test_node_features[sample_index, :num_node, :] = nodes_t[sample_index]
@@ -184,12 +188,10 @@ if __name__ == "__main__":
         torch.tensor(test_adjacency_matrices).type(torch.FloatTensor).to(device)
     )
     test_label = torch.tensor(np.array(test_label)).type(torch.LongTensor).to(device)
-
     "MPNN model"
-
-    model_base = MPNN([100, 100], hidden_state_size=100,
-                      message_size=20, n_layers=1, l_target=50,
-                      type='regression')
+    model_base = MPNN(
+        [100, 100], hidden_state_size=100, message_size=20, n_layers=1, l_target=50, type='regression'
+        )
     model = LinearModel(model_base, 50, len(string_labels)).to(device)
     criterion = nn.NLLLoss()
     evaluation = accuracy
@@ -197,7 +199,6 @@ if __name__ == "__main__":
     model.train()
     losses = []
     best_acc = 0
-
     "Training"
     for step in range(args.num_train_steps):
         train_g, train_h, train_e, train_target = (
@@ -240,3 +241,7 @@ if __name__ == "__main__":
     logging.info(f"Saving model state dict to {args.save_model_to}")
     torch.save(model.state_dict(), args.save_model_to)
     logging.info("Model saved.")
+
+
+if __name__ == "__main__":
+    main()
