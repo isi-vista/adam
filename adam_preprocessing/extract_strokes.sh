f805f5956bbfe2fc00327486301cc822c3a91800
Joe Cecil <jcecil@isi.edu>
2022-09-19 12:16:07 -0400

    Preload Matlab shim when executing stroke extraction

--- adam_preprocessing/extract_strokes.sh
+++ adam_preprocessing/extract_strokes.sh
@@ -4,7 +4,7 @@
 #SBATCH --partition=adam
 #SBATCH --time=4:00:00 # Number of hours required per node, max 24 on SAGA
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=16
+#SBATCH --cpus-per-task=8
 #SBATCH --mem=32g
 #SBATCH --nodes=1
 #SBATCH --mail-type=FAIL,END
@@ -22,4 +22,10 @@ fi
 
 # Because we force there to be only 2 args and then shift by 2, "$@" should expand to nothing.
 # It's included as future-proofing in case we add/allow more args.
-python shape_stroke_extraction.py "$input_curriculum_dir" "$output_curriculum_dir" "$@"
+#
+# Note that because CentOS 7 uses an old version of glibc, we have to preload a shim in order to
+# import the Matlab extension used in the stroke extraction code. This shim provides a definition
+# of __cxa_thread_atexit_impl which is only defined in glibc 2.18+. Without preloading, the
+# extension causes a crash due to an undefined symbol error.
+shim_path=/nas/gaia/adam/matlab/bin/glnxa64/glibc-2.17_shim.so
+LD_PRELOAD="$shim_path" python shape_stroke_extraction.py "$input_curriculum_dir" "$output_curriculum_dir" "$@"
